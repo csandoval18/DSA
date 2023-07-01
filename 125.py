83a4afd0ca49f06def3b62445ff46bd7c4de59b5
Christian Sandoval <christiansandoval1018@outlook.com>
2023-07-01 11:38:21 -0500

    update

--- 125.py
+++ 125.py
@@ -1,24 +1,17 @@
 def isPalindrome(s):
-  # Removes spaces and non alphanumeric values
-  s = ''.join(char.lower() for char in s if char.isalnum())
-  l, r = 0, len(s)-1
+  s = s.lower()
+  s = s.replace(" ", "")
   print(s)
-  print(range(0,(len(s)//2)))
   
-  for i in range(0, (len(s) // 2)):
-    print(l, r)
-    if s[l] != s[r]:
+  l, r = 0, len(s)
+  
+  for i in range(0, len(s) // 2):
+    if l != r:
       return False
-    l += 1
-    r -= 1
+      l += 1
+      r -= 1
   return True
 
-print(isPalindrome(".,"))
+print(isPalindrome("A man, a plan, a canal: Panama"))
   
-def isPalindrome1(s):
-    # Remove non-alphanumeric characters and convert to lowercase
-    s = ''.join(char.lower() for char in s if char.isalnum())
-    
-    # Check if the string is equal to its reverse
-    return s == s[::-1]
-
+  
