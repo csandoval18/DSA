60c43f90d0fc50ea3e48a129376112236386f2c1
Christian Sandoval <christiansandoval1018@outlook.com>
2023-07-07 14:10:56 -0500

    update

--- 383.py
+++ 383.py
@@ -1,8 +1,11 @@
 class Solution(object):
     def canConstruct(self, ransomNote, magazine):
       magazine = {}
+      return
       
-      
-      
-      
+ans = Solution()
 
+ransomNote = "aa"
+magazine = "aab"
+
+print(ans.canConstruct(ransomNote, magazine))
