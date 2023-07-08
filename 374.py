e1ac0706c1f38a134dc09cc2054c1cceb68e7e4b
Christian Sandoval <christiansandoval1018@outlook.com>
2023-07-08 10:28:06 -0500

    update

--- 374.py
+++ 374.py
@@ -1,17 +1,36 @@
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num):
+
+# Test case:
+# Input: n = 10, pick = 6
+# Output: 6
+
+# Use binary search to find pick
+
+
 class Solution(object):
   def guessNumber(self, n):
-    self.binarySearchHelper(1, n)
-  
-    def binarySearch(l, r):
-      # base case 
-      if l <= r:
-        m = (l + r) // 2
-        
-        if m == x:
-          return mid
+    ans = self.binarySearch(1, n)
+    return ans
+    
+  def binarySearch(l, r):
+    # base case 
+    if l <= r:
+      m = (l + r) // 2
+      x = guess(m)
       
+      if x == 0:
+        return m
       
-
-        
-      return
+      if x == 1:
+        return self.binarySearch(m, r)
+      
+      if x == -1:
+        return self.binarySearch(l, m)
+    else: return -1    
+    
       
