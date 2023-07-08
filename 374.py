2ed744183ef44edb57180f4f89ccbca716632134
Christian Sandoval <christiansandoval1018@outlook.com>
2023-07-08 00:15:57 -0500

    update

---
+++ 374.py
@@ -0,0 +1,17 @@
+class Solution(object):
+  def guessNumber(self, n):
+    self.binarySearchHelper(1, n)
+  
+    def binarySearch(l, r):
+      # base case 
+      if l <= r:
+        m = (l + r) // 2
+        
+        if m == x:
+          return mid
+      
+      
+
+        
+      return
+      
