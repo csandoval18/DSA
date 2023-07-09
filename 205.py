ae8cffd2e0baa4456e4d08c41887ba9dc8796c76
Christian Sandoval <christiansandoval1018@outlook.com>
2023-07-09 13:38:33 -0500

    update

--- 205.py
+++ 205.py
@@ -16,3 +16,29 @@ t = "add"
 
 ans = Solution()
 print(ans.isIsomorphic(s, t))
+
+
+def isIsomorphic(s, t):
+  if len(s) != len(t):
+    return False
+  
+  hm = {}
+  rev = {}
+  
+  for i in range(len(s)):
+    char_s, char_t = s[i], t[i]
+    
+    # Check mapping from s to t
+    if char_s in hm:
+      if hm[char_s] != char_t:
+        return False
+    else:
+      hm[char_s] = char_t
+    
+    # Check mapping from t to s
+    if char_t in rev:
+      if rev[char_t] != char_s:
+          return False
+    else:
+      rev[char_t] = char_s
+  return True
