a238382aab162043fa1949d5d8ef57cc89e5ab97
Christian Sandoval <christiansandoval1018@outlook.com>
2023-07-09 02:29:45 -0500

    update

--- 205.py
+++ 205.py
@@ -1,3 +1,16 @@
 class Solution(object):
   def isIsomorphic(self, s, t):
-     
+    hm = {}
+    
+    for i in range(len(t)):
+      if s[i] not in hm:
+        hm[t[i]] = s[i]
+      else:
+        return False
+    return True
+
+s = "foo"
+t = "bar"
+
+ans = Solution()
+print(ans.isIsomorphic(s, t))
