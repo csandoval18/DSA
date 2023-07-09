5a61eea6a029cdcedf11db00cf75950c81d509c6
csandoval18 <christiansandoval1018@outlook.com>
2023-07-09 10:41:12 -0500

    update

--- 205.py
+++ 205.py
@@ -1,16 +1,18 @@
 class Solution(object):
   def isIsomorphic(self, s, t):
     hm = {}
+    rev = {}
     
     for i in range(len(t)):
-      if s[i] not in hm:
-        hm[t[i]] = s[i]
+      if s[i] not in hm and t[i] not in rev:
+        hm[s[i]] = t[i]
+        rev[t[i]] = s[i]
       else:
         return False
     return True
 
-s = "foo"
-t = "bar"
+s = "egg"
+t = "add"
 
 ans = Solution()
 print(ans.isIsomorphic(s, t))
