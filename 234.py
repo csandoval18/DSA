abe6343170b4bd9eecfe417af319e28cea973662
Christian Sandoval <christiansandoval1018@outlook.com>
2023-07-10 20:32:01 -0500

    update

---
+++ 234.py
@@ -0,0 +1,3 @@
+class Solution(object):
+  def isPalindrome(self, head):
+    return
--- 414.py
+++ 414.py
@@ -1,8 +1,54 @@
 class Solution(object):
-    def isPowerOfTwo(self, n):
-      i = 0
-      while pow(2, i) < n:
-        i += 1
-        
-      return True if pow(2, i) == n else False
-      
+    def thirdMax(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        res = []
+    
+        if len(nums) >= 3:
+          for i in range(len(nums)):
+              m = max(nums)
+              res.append(m)
+              nums.remove(m)
+          return max(res[2])
+        else:
+          return max(nums)
+          
+class Solution(object):
+  def thirdMax(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    hm = {}
+    
+    # initialize 3 variables for 3 max numbers
+    # they are initialized to float(-inf) tohandle the case where there are no 
+    # distinct max numbers in the array
+    
+    first = second = third = float('-inf')
+
+    # For each number we compare it with the current top 3 max nums
+    # If it is greater than first, we update all 3 variables to maintain the ordering of the top 3 nums
+    
+    for n in nums:
+        if n > first:
+            third = second
+            second = first
+            first = n
+        elif n > second and n < first:
+            third = second
+            second = n
+        elif n > third and n< second:
+            third = n
+    
+    if third != float('-inf'):
+        return third
+    else:
+        return first
+    
+    
+    
+    
