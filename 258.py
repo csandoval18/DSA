8ad8b595ed4f26e0fdcffe61a5539e62b9b6460b
Christian Sandoval <christiansandoval1018@outlook.com>
2023-07-18 14:47:41 -0500

    update

--- 258.py
+++ 258.py
@@ -1,7 +1,19 @@
 class Solution(object):
-  def addDigits(self, num):
-    arr = []
-    x = num
-    while x != 0:
-      arr = [x%10] + arr
-      x = x // 10
+    def addDigits(self, num):
+      if num % 10 != 0 and num // 10 == 0:
+        return num
+      elif num == 0:
+        return 0
+        
+      arr = []
+      tmp = num
+      
+      while tmp != 0:
+        arr = [tmp % 10] + arr
+        tmp = tmp // 10
+      
+      total = 0
+      for n in arr:
+        total += n
+      
+      self.addDigits(total)
