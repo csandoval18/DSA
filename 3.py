223a20fad59504beaaa05cfaf404dbd252421280
Christian Sandoval <christiansandoval1018@outlook.com>
2023-08-16 21:00:30 -0500

    update

---
+++ 3.py
@@ -0,0 +1,14 @@
+class Solution(object):
+  def lengthOfLongestSubstring(self, s):
+    res = ""
+    
+    for l in s:
+      print(l)
+    return 0
+
+# Input: s = "abcabcbb"
+# Output: 3
+
+s = "abcabcbb"
+sol = Solution()
+sol.lengthOfLongestSubstring(s)
--- 5.py
+++ 5.py
@@ -1,15 +1,37 @@
+# Brute force
 class Solution(object):
+  
+  def isPalindrome(self, s):
+    return s == s[::-1]
+  
   def longestPalindrome(self, s):
     res = ""
     l = 0
     r = len(s)-1
     
-    for n, letter in enumerate(s):
-      while l <= r:
-        m = (l + r) // 2
+    
+    for i in range(len(s)):
+      for j in range(i+1, len(s) + 1):
+        if self.isPalindrome(s[i:j]) and j - i > len(res):
+          res = s[i:j]
+    return res
+            
+            
+
+  # 2 Pointer solution 
+  def  longestPalindrome2(self, s):
+    res = ""
+    resLen = 0
+    
+    for i in range(len(s)):
+      # odd length
+      l, r = i, i
+      while l >= 0 and r < len(s) and s[l] == s[r]:
         
         
         
+      
+  
 
 s = "babad"
 sol = Solution()
