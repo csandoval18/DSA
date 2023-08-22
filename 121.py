b4ce0401ec0ceb5836243a780d677c1c9d60c3d3
Christian Sandoval <christiansandoval1018@outlook.com>
2023-08-21 20:30:52 -0500

    update

--- 121.py
+++ 121.py
@@ -4,26 +4,20 @@ def maxProfit1(prices):
   profit = 0
   
   for i in range(len(prices)):
-    print("i:", i)
-    for j in range(i, len(prices)):
-      print("j:", j)
-      profit = prices[j] - prices[i] 
+    for j in range(i+1, len(prices)):
+      profit = prices[j]-prices[i] 
+      
       if profit > maxProfit:
         maxProfit = profit
   
   return maxProfit
-  
-# prices = [1,2]
-prices = [7,1,5,3,6,4]
-print(maxProfit(prices))
 
-
-def maxProfit(self, prices):
+def maxProfit(prices):
   l, r = 0, 1
   maxP = 0
   
   while r < len(prices):
-    # Check left and right pointers values. If right pointer is greater than left (profit exists)
+    # Check left and right pointer values. If right pointer is greater than left (profit exists)
     if prices[l] < prices[r]:
       # then calc profit
       profit = prices[r] - prices[l]
@@ -32,7 +26,10 @@ def maxProfit(self, prices):
     else:
       # move left pointer to right position
       l = r
-      
-    # move 
     r += 1
   return maxP
+  
+# prices = [1,2]
+prices = [7,1,5,3,6,4]
+print(maxProfit1(prices))
+print(maxProfit(prices))
