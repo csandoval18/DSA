7897531f97aa127efd9651606a23c776d67b132b
csandoval18 <christiansandoval1018@outlook.com>
2023-08-25 15:13:52 -0500

    update

---
+++ 53.py
@@ -0,0 +1,9 @@
+def maxSubArray(self, nums):
+  max_sum = float('-inf')
+  current_sum = 0
+  
+  for n in nums:
+    current_sum = max(n, current_sum + n)
+    max_sum = max(max_sum, current_sum)
+  
+  return max_sum
