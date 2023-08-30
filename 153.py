671dd981bc6da2e4f1105ec339d2fb0f7488b855
csandoval18 <christiansandoval1018@outlook.com>
2023-08-30 01:43:14 -0500

    update

--- 153.py
+++ 153.py
@@ -1,15 +1,18 @@
-def findMin(nums):
+def findMinBruteForce(nums):
+  minVal = float('inf')
+  for n in nums:
+    if n < minVal:
+      minVal = n
+  return minVal
+
+def findMinAttempt(nums):
   l = 0
   r = len(nums)-1
   
   while l <= r:
-    print("l:", l)
-    print("r:", r)
     m = (l + r) // 2
-    print("m:", m)
-    print("\n")
     
-    if nums[l] > nums[r]:
+    if nums[l] > nums[r] and nums[m-1] > nums[m]:
       l = m
     elif nums[l] < nums[r]:
       r = m
@@ -22,4 +25,37 @@ def findMin(nums):
 # nums = [4,5,6,7,0,1,2]
 # nums = [11,13,15,17]
 # nums = [2,3,1]
-print(findMin(nums))
+
+# This case breaks previous algo without "and nums[m-1] > nums[m]"
+#         L   M   R
+# nums = [5,1,2,3,4]
+
+#             L   R
+# nums = [5,1,2,3,4]
+
+# //////////////////////////////////////////////////////
+
+def findMin(nums):
+  minVal =  float('inf')
+  l, r = 0, len(nums)-1
+  
+  while l <= r:
+    m = (l+r) // 2
+    
+    # Get min in sorted half
+    if nums[l] <= nums[m]: 
+        minVal = min(minVal, nums[l])
+        l = m + 1
+    elif nums[m] <= nums[r]: 
+        minVal = min(minVal, nums[m])
+        r = m - 1
+        
+  return minVal
+    
+    # Perform binary search in non sorted half
+    
+    
+    
+      
+
+# print(findMin(nums))
