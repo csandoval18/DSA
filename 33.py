609de5b9a5627a551fb48d051bf9c3b4f2a82df8
csandoval18 <christiansandoval1018@outlook.com>
2023-08-30 19:30:11 -0500

    update

--- 33.py
+++ 33.py
@@ -5,9 +5,25 @@ def search(nums, target):
   while l <= r:
     m = (l+r) // 2
     
-    # base case
+    # Base case
     if nums[m] == target:
       return m
-      
     
-  
+    # Check if left half is sorted
+    if nums[l] <= nums[m]:
+      # Check if target is within left sorted half
+      if nums[l] <= target and target <= nums[m]:
+        r = m - 1
+      # Else target could be in right non-sorted side
+      else:
+        l = m + 1
+    elif nums[m] <= nums[r]:
+      # Check if target is within right sorted half
+      if nums[m] <= target and target <= nums[r]:
+        l = m + 1
+      # Else target could be in left non-sorted half
+      else:
+        r = m - 1
+        
+  return -1     
+    
