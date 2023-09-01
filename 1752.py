c3110335fca8d7c86bfe6eab6c60e2d5ee789b9a
csandoval18 <christiansandoval1018@outlook.com>
2023-08-31 23:02:26 -0500

    update

--- 1752.py
+++ 1752.py
@@ -8,10 +8,23 @@ def checkFirstAttempt(nums):
       return True
   return True
   
+# First attempt breaks with array below 
+# nums = [8,5,4,5,1,4,5,2,2]
+
+def check(nums):
+  increasing_count = 0
+  
+  for i in range(len(nums)):
+    if nums[i] > nums[(i+1) % len(nums)]:
+      increasing_count += 1
+      if increasing_count > 1:
+        return False
+  return True if increasing_count == 0 or nums[0] >= nums[n-1] else False
+
+  
 # nums = [3,4,5,1,2]
 # nums = [2,1,3,4]
 # nums = [1,2,3,4,5]
-# First attempt breaks with array below 
-nums = [8,5,4,5,1,4,5,2,2]
+# nums = [8,5,4,5,1,4,5,2,2]
 
-print(check(nums))
+# print(check(nums))
