eafd5f61809d37164884fadd99815b7884fd8cac
csandoval18 <christiansandoval1018@outlook.com>
2023-09-02 18:16:51 -0500

    update

--- utils/missingNumber.py
+++ utils/missingNumber.py
@@ -1,3 +1,4 @@
+# LC doesnt accept
 def missingNumber(nums):
   nums.sort()
   tmp = nums[0]
@@ -7,6 +8,16 @@ def missingNumber(nums):
       return tmp+1
     else:
       tmp = nums[i]
+
+def missing_number(nums):
+  i = 0
+  
+  for n in nums:
+    if i != n:
+      return i
+    i += 1
+  return i
+  
 
 nums = [3,0,1]
 print(missingNumber(nums))
