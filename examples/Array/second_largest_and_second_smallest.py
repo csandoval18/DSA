78b9d2a4352c7f8429d4deda36870659e766d694
csandoval18 <christiansandoval1018@outlook.com>
2023-09-10 14:48:48 -0500

    update

--- examples/Array/second_largest_and_second_smallest.py
+++ examples/Array/second_largest_and_second_smallest.py
@@ -1,3 +1,27 @@
+def getElements(arr, n):
+    if n == 0 or n == 1:
+        print(-1, -1)  # edge case when only one element is present in array
+        
+    small = float('inf')
+    second_small = float('inf')
+    large = float('-inf')
+    second_large = float('-inf')
+    
+    for i in range(n):
+        small = min(small, arr[i])
+        large = max(large, arr[i])
+        
+    for i in range(n):
+        if arr[i] < second_small and arr[i] != small:
+            second_small = arr[i]
+        if arr[i] > second_large and arr[i] != large:
+            second_large = arr[i]
+            
+    return [second_large, second_small]
+    
+    
+    
+    
 def getSecondOrderElements(n, arr):
   def secondSmallest(arr, n):
     if n < 2:
@@ -11,6 +35,8 @@ def getSecondOrderElements(n, arr):
         second_small = small
         small = arr[i]
 
+      # We can just check if arr[i] != small too to update just second_small
+      # elif small != arr[i] and arr[i] < second_small:
       elif small < arr[i] and arr[i] < second_small:
         second_small = arr[i]
     
@@ -27,6 +53,8 @@ def getSecondOrderElements(n, arr):
       if largest < arr[i]:
         secondLargest = largest
         largest = arr[i]
+      # We can just check if arr[i] != small too to update just second_small
+      # elif secondLargest < arr[i] and arr[i] != largest:
       elif secondLargest < arr[i] and arr[i] < largest:
         secondLargest = arr[i]
 
