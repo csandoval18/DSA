a062d542c10ceb4b6a874dc10004fac7218feefc
csandoval18 <christiansandoval1018@outlook.com>
2023-09-11 22:46:51 -0500

    update

---
+++ 229.py
@@ -0,0 +1,7 @@
+def majorityElement(nums):
+  
+  return
+  
+# Return all elements that appear more than ⌊ n/3 ⌋ (floor) | (ceil) = ⌈ x ⌉
+nums = [3,2,3]
+print(majorityElement(majorityElement(nums)))
--- examples/Dynamic Programming/climbing_stairs.py
+++ examples/Dynamic Programming/climbing_stairs.py
@@ -6,7 +6,7 @@ def climbStairs(n):
   # Initialize an array to store the number of ways to reach each step
   # We begin the array with 0 depite not being useful for the solution 
   # to maintain the index = to the step # for consistency purposes
-  dp = [0] * n
+  dp = [0] * (n+1)
   dp[1] = 1 # There is one way to reach the first step
   dp[2] = 2 # There are two ways to reach the second step
   
