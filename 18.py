34a170d6b240a106c5b4cce8eea531696779bf0b
csandoval18 <christiansandoval1018@outlook.com>
2023-09-13 12:10:05 -0500

    update

--- 18.py
+++ 18.py
@@ -2,29 +2,35 @@
 def fourSum(nums, target):
   n = len(nums)
   res = []
+  nums.sort()
   
-  for i in range(n-3):
-    if i != 0 and nums[i] != nums[j]:
+  for i in range(n):
+    if i != 0 and nums[i] == nums[i-1]:
       continue
       
-    for j in range(i+1, n-2): 
+    for j in range(i+1, n): 
       if j != i+1 and nums[j] == nums[j-1]:
         continue
       
-      k =j+1
+      k = j+1
       l = n-1
       
       while k < l:
         curr_sum = nums[i] + nums[j] + nums[k] + nums[l]
         
-        if curr_sum < 0:
+        if curr_sum < target:
           k += 1
-        elif curr_sum > 0:
+        elif curr_sum > target:
           l -= 1
         else:
           res.append([nums[i], nums[j], nums[k], nums[l]])
           k += 1
           l -= 1
+          
+          while k < l and nums[k] == nums[k-1]:
+            k += 1
+          while k < l and nums[l] == nums[l+1]:
+            l -= 1
           
   return res
 
