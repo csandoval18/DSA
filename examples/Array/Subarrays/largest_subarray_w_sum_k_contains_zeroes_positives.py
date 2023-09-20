e6ead7fd2576a488f6f53f541197da45eb8f8788
csandoval18 <christiansandoval1018@outlook.com>
2023-09-20 12:54:04 -0500

    update

--- examples/Array/Subarrays/largest_subarray_w_sum_k_contains_zeroes_positives.py
+++ examples/Array/Subarrays/largest_subarray_w_sum_k_contains_zeroes_positives.py
@@ -41,7 +41,31 @@ def longestSubarrayWithSumk(a: [int], k: int) -> int:
   return maxLen
     
 
-      
+def getLongestSubarray(nums: [int], k: int) -> int:
+  # Write your code here
+  left = 0
+  right = 0
+  currSum = 0
+  maxLen = 0
+
+  while right < len(nums):
+    currSum += nums[right]
+    # For case when currSum > k and we must decrement to reach k or a 
+    # number below k. Make sure left <= right
+    while left <= right and currSum > k:
+      currSum -= nums[left]
+      left += 1
+
+    # If sum of k found, then that means there is a subarray with sum 6
+    # between range(left, right+1)
+    if currSum == k:
+      maxLen = max(maxLen, right-left+1)
+    
+    # Increase right to next element and add element value
+    right += 1
+
+  return maxLen
+    
 a = [1,2,3,1,1,1,1]
 # a = [2,2,4,1,2]
 k = 3
