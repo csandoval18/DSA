3c1f38e1ae5afb788135b1aad10ca1b6b6b1e59e
csandoval18 <christiansandoval1018@outlook.com>
2023-09-21 16:58:24 -0500

    update

--- examples/Recursion/reverse_array.py
+++ examples/Recursion/reverse_array.py
@@ -1,6 +1,6 @@
 def reverse_array(arr):
     if len(arr) <= 1:
-        return arr  # Base case: an array with 0 or 1 element is already reversed
+      return arr  # Base case: an array with 0 or 1 element is already reversed
 
     # Recursive step: reverse the subarray excluding the first element,
     # then append the first element at the end.
@@ -12,15 +12,16 @@ reversed_array = reverse_array(original_array)
 print(reversed_array)  # Output: [5, 4, 3, 2, 1]
 
 
-def reverse_array_recursive(arr, start, end):
-    if start >= end:
-        return  # Base case: nothing to reverse
+def reverse_array_recursive(arr, left, right):
+    if left >= right:
+      return  # Base case: nothing to reverse
     
-    # Swap the elements at the start and end indices
-    arr[start], arr[end] = arr[end], arr[start]
+    # Swap the elements at the left and right indices
+    arr[left], arr[right] = arr[right], arr[left]
 
-    # Recursive step: move the start index forward and the end index backward
-    reverse_array_recursive(arr, start + 1, end - 1)
+    # Recursive step: move the left index forward and the right index backward
+    # Return not needed it seems
+    return reverse_array_recursive(arr, left + 1, right - 1)
 
 # Example usage:
 original_array = [1, 2, 3, 4, 5]
