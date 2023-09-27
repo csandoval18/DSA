d60b0e9fe20ced308e3425afe5356ebf904af5f9
csandoval18 <christiansandoval1018@outlook.com>
2023-09-26 22:16:54 -0500

    update

---
+++ 278.py
@@ -0,0 +1,27 @@
+
+def isBadVersion(version):
+  bad = 4
+  
+  if version < 4: return False
+  else: return True
+  
+  
+
+def firstBadVersion(n):
+  l = 1
+  r = n
+  
+  while l <= r:
+    m = (l+r)//2
+    
+    mVer = isBadVersion(m)
+    if mVer == True and isBadVersion(m-1):
+      return m
+    elif isBadVersion(l) == False and mVer == False:
+      l = m+1
+    elif mVer == True and isBadVersion(r) == True:
+      r = m-1
+    
+  return -1
+  
+      
