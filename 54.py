4d6c953c50affc30e8831075e119581660f6ecad
csandoval18 <christiansandoval1018@outlook.com>
2023-10-21 22:37:53 -0500

    update

--- 54.py
+++ 54.py
@@ -8,4 +8,36 @@
 # Output: [1,2,3,6,9,8,7,4,5]
 
 def spiralOrder(matrix):
+  n = len(matrix)
+  m = len(matrix[0])
+  left = 0
+  right = m-1
+  top = 0
+  bottom = n-1
+  res = []
   
+  while top <= bottom and left <= right:
+    for i in  range(left, right+1):
+      res.append(matrix[top][i])
+    top += 1
+    
+    for i in range(top, bottom+1):
+      res.append(matrix[i][right])
+    right -= 1
+    
+    if top <= bottom:
+      for i in range(right, left-1, -1):
+        res.append(matrix[bottom][i])
+      bottom -= 1
+
+    if left <= right:
+      for i in range(bottom, top-1, -1):
+        res.append(matrix[i][left])
+      left += 1
+  
+  return res
+    
+    
+    
+    
+    
