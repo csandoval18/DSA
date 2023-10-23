223761af4bebfb2472c5c64afdc13866a5a4f0e3
csandoval18 <christiansandoval1018@outlook.com>
2023-10-23 00:01:36 -0500

    update

--- 560.py
+++ 560.py
@@ -18,7 +18,7 @@ def subarraySumBF(nums, k):
   for i in range(n):
     for j in range(i,n):
       currSum = 0
-      for k in range(i, j):
+      for k in range(i, j+1):
         currSum += nums[k]
 
       if currSum == k:
@@ -27,6 +27,32 @@ def subarraySumBF(nums, k):
   return count
   
 def subarraySumBetter(nums, k):
+  n = len(nums)
+  count = 0
+  
+  for i in range(n):
+    curr_sum = 0
+    for j in range(i, n):
+      curr_sum += nums[j]
+    
+    if curr_sum == k:
+      count += 1
+    
+  return count
+  
+def subarraySumOP(nums, k):
+  hm = {0:1}
+  curr_sum = 0
+  count = 0
+
+  for i in range(len(nums)): 
+    curr_sum += nums[i]
+    if curr_sum - k in hm:
+      count += hm[curr_sum-k]
+    hm[curr_sum] = hm.get(curr_sum, 0) + 1
+  
+  return count
+  
 # nums = [1,1,1]
 # k = 2
 # How many subarrays add up to k?
