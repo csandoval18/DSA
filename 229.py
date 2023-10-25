795bbffa39f8dc318ec766cb32cfd6bc8c902f89
csandoval18 <christiansandoval1018@outlook.com>
2023-10-25 16:16:50 -0500

    update

--- 229.py
+++ 229.py
@@ -19,18 +19,49 @@ def majorityElementBF(nums):
       break
   return res
   
+#TUF using collections counter
+from collections import Counter
+
+def majorityElementTUF(nums):
+  # Size of the given array
+  n = len(nums)
   
-def majorityElementBetter(nums):
+  # Count the occurrences of each element using Counter
+  counter = Counter(nums)
+  # Searching for the majority element
+  for num, count in counter.items():
+    if count > n // 3:
+      return num
+  
+  return -1
+  
+  
+# O(n+m)
+def majorityElementBetterOwnImplementaition(nums):
+  n = len(nums)
   hm = {}
+  res = []
   
+  # O(n)
   for num in nums:
     hm[num] = hm.get(num, 0) + 1
-    
-  return hm
-    
   
+  # O(m) where 'm' = # of unique values
+  for key, count in hm.items():
+    if count > n // 3:
+      res.append(key)
+    
+    if len(res) == 2:
+      break
+    
+  return res
   
 # Return all elements that appear more than ⌊ n/3 ⌋ (floor) | (ceil) = ⌈ x ⌉
-nums = [3,2,3]
+# nums = [3,2,3]
+nums = [1,2]
 # print(majorityElement(majorityElement(nums)))
-print(majorityElementBetter(nums))
+print(majorityElementBetterOwnImplementaition(nums))
+
+def majorityElementOP(nums):
+  
+  return 0
