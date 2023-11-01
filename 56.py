1f1fd0cc2a28443cfd5c4a723d2d13d19f65b05d
csandoval18 <christiansandoval1018@outlook.com>
2023-10-31 23:36:14 -0500

    update

--- 56.py
+++ 56.py
@@ -8,11 +8,28 @@ def mergeOverlappingIntervals(intervals):
   n = len(intervals)
   ans = []
   
+  # 1. Sort the closer intervals [X, 0]
   intervals.sort()
   
   for i in range(n):
+    # 2. Select one interval 
+    # [start,end] format
     start, end = intervals[i][0], intervals[i][1]
     
     # Skip all the merged intervals:
-    if ans and end <= ans[-1][1]
+    if ans and end <= ans[-1][1]:
+      continue
+      
+    for j in range(n):
+      if intervals[j][0] <= end:
+        end = max(end, intervals[j][1])
+      else:
+        break
+      
+    ans.append([start, end])
+
+  return ans
+
+
+      
   
