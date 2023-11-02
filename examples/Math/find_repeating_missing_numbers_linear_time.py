0722e3b5fd44863b5f882182464f11f377bc289d
csandoval18 <christiansandoval1018@outlook.com>
2023-11-02 15:41:01 -0500

    update

---
+++ examples/Math/find_repeating_missing_numbers_linear_time.py
@@ -0,0 +1,30 @@
+def findMissingRepeatingNumbers(a):
+  n = len(a)
+  
+  # Find Sn and S2n
+  SN = (n * (n+1)) // 2
+  S2N = (n * (n+1) * (2 * n + 1)) // 6
+  
+  # Calculate S and S2
+  S, S2 = 0, 0
+  for i in range(n):
+    S += a[i]
+    S2 += a[i] * a[i]
+  
+  # S-Sn = X-Y:
+  val1 = S - SN
+  
+  # S2-S2n = X^2-Y^2
+  val2 = S2 - S2N
+  
+  # Find X+Y = (X^2-Y^2)/(X-Y)
+  val2 = val2 // val1
+  
+  # Find X and Y: X = ((X+Y)+(X-Y))/2 and Y = X-(X-Y),
+  # Here, X-Y = val1 and X+Y = val2
+  x = (val1 + val2) // 2
+  y = x - val1
+  
+  return [x, y]
+  
+  
