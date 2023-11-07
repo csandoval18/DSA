9512e8a6c550f215f85753a4b5ca354c6f7692d2
csandoval18 <christiansandoval1018@outlook.com>
2023-11-07 14:19:26 -0600

    update

--- 493.py
+++ 493.py
@@ -1,7 +1,7 @@
 def reversePairs(nums: [int]) -> int:
   def mergeSort(nums, l, r):
     if l >= r:
-      return
+      return count
     
     m = (l+r) // 2
     count = mergeSort(nums, l, m) + mergeSort(nums, m+1, r)
@@ -20,3 +20,25 @@ def reversePairs(nums: [int]) -> int:
   
 nums = [1,3,2,3,1]
 reversePairs(nums)
+
+
+
+class Solution:
+    def reversePairs(self, nums: List[int]) -> int:
+        def mergeSort(nums, start, end):
+            if start >= end:
+                return 0
+
+            mid = (start + end) // 2
+            count = mergeSort(nums, start, mid) + mergeSort(nums, mid + 1, end)
+
+            j = mid + 1
+            for i in range(start, mid + 1):
+                while j <= end and nums[i] > 2 * nums[j]:
+                    j += 1
+                count += j - (mid + 1)
+
+            nums[start:end + 1] = sorted(nums[start:end + 1])
+            return count
+
+        return mergeSort(nums, 0, len(nums) - 1)
