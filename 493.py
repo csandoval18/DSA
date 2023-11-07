d3801a4d8186a8dd9bfc0a1e68d1bdf52d70b258
csandoval18 <christiansandoval1018@outlook.com>
2023-11-07 11:35:14 -0600

    update

--- 493.py
+++ 493.py
@@ -1,10 +1,22 @@
 def reversePairs(nums: [int]) -> int:
-  if len(nums) == 1:
-    return
+  def mergeSort(nums, l, r):
+    if l >= r:
+      return
+    
+    m = (l+r) // 2
+    count = mergeSort(nums, l, m) + mergeSort(nums, m+1, r)
+    
+    j = m+1
+    for i in range(l, m+1):
+      while j <= r and nums[i] > 2 * nums[j]:
+        j += 1
+      count += j - (m+1)
+      
+    nums[l:r+1]  = sorted(nums[l:r + 1])
+    return count
+    
+  return mergeSort(nums, 0, len(nums) - 1)
+    
   
-  reversePairs()
-  
-
-def merge(arr, l, m, r):
-  tmp = []
-  
+nums = [1,3,2,3,1]
+reversePairs(nums)
