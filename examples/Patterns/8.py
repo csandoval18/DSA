9afd2ee01632402b201221339117c0a0d487294f
csandoval18 <christiansandoval1018@outlook.com>
2023-11-13 23:27:53 -0600

    update

---
+++ examples/Patterns/8.py
@@ -0,0 +1,17 @@
+# Pattern 7
+
+#   *
+#  ***
+# *****
+
+def nNumberTriangle(n: int) -> None:
+  for i in range(n-i-1):
+    for j in range(i):
+      print(" ", end="")
+    for j in range(2*i+1):
+      print("*", end="")
+    for j in range(n-i-1):
+      print(" ", end="")
+      
+nNumberTriangle(3)
+  
