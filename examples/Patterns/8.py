0d4958b3aed50ef8f3c592697a448dbfbec2e9bd
csandoval18 <christiansandoval1018@outlook.com>
2023-11-15 00:09:41 -0600

    update

--- examples/Patterns/8.py
+++ examples/Patterns/8.py
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-# Pattern 7
-
-#   *
-#  ***
-# *****
-
-def nNumberTriangle(n: int) -> None:
-  for i in range(n-i-1):
-    for j in range(i):
-      print(" ", end="")
-    for j in range(2*i+1):
-      print("*", end="")
-    for j in range(n-i-1):
-      print(" ", end="")
-      
-nNumberTriangle(3)
-=======
 # Pattern 8
 
 # *****
@@ -32,7 +14,4 @@ def nStarTriangle(n: int) -> None:
     #   print(" ", end="")
     print()
 
-      
 nStarTriangle(3)
->>>>>>> 3d75a4588b11131016e4d3ccdf523c54a9bf6615
-  
