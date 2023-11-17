5a51675f9f51ec0e749abb93ce2c6caa05243878
csandoval18 <christiansandoval1018@outlook.com>
2023-11-16 20:00:56 -0600

    update

--- examples/Patterns/13.py
+++ examples/Patterns/13.py
@@ -2,7 +2,8 @@ def nNumberTriangle(n: int) -> None:
   x = 1
   for i in range(n):
     for j in range(i+1):
-      print(x, end="")
+      print(x, end=" ")
+      x += 1
     print()
 
 nNumberTriangle(3)
