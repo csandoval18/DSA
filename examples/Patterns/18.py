bf1eb3e4b71375195dcd5788b8ac472912d0694d
csandoval18 <christiansandoval1018@outlook.com>
2023-11-18 16:52:18 -0600

    update

---
+++ examples/Patterns/18.py
@@ -0,0 +1,25 @@
+# n=3
+# C
+# C B 
+# C B A
+
+# n=1
+# C
+
+# def alphaTriangle(n: int) -> None:
+#   for i in range(n):
+#     for j in range(i+1):
+#       print(chr(ord('A') - i + j), end=" ")
+#     print()
+  
+# alphaTriangle(3)
+
+def alphaTriangle(n: int) -> None:
+  char = 'A'
+  
+  for i in range(n):
+    for j in range(i+1):
+      print(chr(ord(char)+n-1-j), end=" ")
+    print()
+
+alphaTriangle(3)
