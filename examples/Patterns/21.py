b30a573509d1943dfa61132e3c9889207d057d2c
csandoval18 <christiansandoval1018@outlook.com>
2023-11-18 22:11:09 -0600

    update

---
+++ examples/Patterns/21.py
@@ -0,0 +1,26 @@
+# n = 3
+
+# ***
+# * *
+# ***
+
+# n=5
+
+# *****
+# *   *
+# *   *
+# *   *
+# *****
+
+def getStarPattern(n: int) -> None:
+  for i in range(n):
+    for j in range(n):
+      if i==0 or i==n-1 or j==0 or j==n-1:
+        print("*", end="")
+      else:
+        print(" ", end="")
+    print()
+    
+getStarPattern(3) 
+
+    
