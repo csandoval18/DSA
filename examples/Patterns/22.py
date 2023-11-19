7473727f5b525c8070061da340d32cbc6ded62b2
csandoval18 <christiansandoval1018@outlook.com>
2023-11-18 23:33:15 -0600

    update

---
+++ examples/Patterns/22.py
@@ -0,0 +1,28 @@
+# n=3
+# 33333
+# 32223
+# 32123
+# 33333
+
+# n=4
+# 4444444
+# 4333334
+# 4322234
+# 4321234
+# 4322234
+# 4333334
+# 4444444
+
+def getNumberPattern(n: int) -> None:
+  for i in range(2*n-1):
+    for j in range(2*n-1):
+      top = i
+      bottom = j
+      right = (2*n-2) - j
+      left = (2*n-2) - i
+      
+      print(n- min(min(top, bottom), min(left, right)), end=" ")
+  print() 
+
+
+    
