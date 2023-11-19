c354d11721edc7e3bd86292fe405eb95a6fa68f3
csandoval18 <christiansandoval1018@outlook.com>
2023-11-18 20:57:16 -0600

    update

--- examples/Patterns/19.py
+++ examples/Patterns/19.py
@@ -9,16 +9,16 @@ def symmetry(n: int) -> None:
   for i in range(n):
     for j in range(n-i):
       print("*", end=" ")
-    for j in range(2*i):
+    for j in range(1, 2*i+1):
       print(" ", end=" ")
     for j in range(n-i):
       print("*", end=" ")
     print()
     
-  for i in range(n):
+  for i in range(1, n):
     for j in range(i+1):
       print("*", end=" ")
-    for j in range(2*n - (2*i+2)):
+    for j in range(1, 2*n - (2*i+1)):
       print(" ", end=" ")
     for j in range(i+1): 
       print("*", end=" ")
---
+++ examples/Patterns/20.py
@@ -0,0 +1,26 @@
+# *         *
+# * *     * *
+# * * * * * *
+# * *     * *
+# *         *
+
+def symmetry(n: int) -> None:
+  for i in range(n):
+    for j in range(i+1):
+      print("*", end=" ")
+    for j in range(1, 2*n-(2*i+1)):
+      print(" ", end=" ")
+    for j in range(i+1):
+      print("*", end=" ")
+    print()
+    
+  for i in range(n):
+    for j in range(n-i):
+      print("*", end=" ")
+    for j in range(1, 2*i+1):
+      print(" ", end=" ")
+    for j in range(n-i):
+      print("*", end=" ")
+    print() 
+
+symmetry(3)
