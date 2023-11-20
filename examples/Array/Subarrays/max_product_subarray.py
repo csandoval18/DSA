86882fa12061c676c6887c3554b0baf7f9257a7f
csandoval18 <christiansandoval1018@outlook.com>
2023-11-19 19:39:35 -0600

    update

--- examples/Array/Subarrays/max_product_subarray.py
+++ examples/Array/Subarrays/max_product_subarray.py
@@ -2,10 +2,20 @@ from typing import List
 
 def maxProduct(nums: List) -> int:
   n = len(nums)
+  pre, suf = 1, 1
+  maxProd = float('-inf')
+  
   for i in range(n):
-    for j in range(i, n):
-      print(nums[j], end="->")
-    print()
-        
+    if pre == 0:
+      pre = 1
+    if suf == 0:
+      suf = 1
+    
+    pre = pre * nums[i]
+    suf = suf * arr[n-i-1]
+    maxProd = max(maxProd, pre, suf)
+  return maxProd
+    
 arr = [2,3,-2,4]
-maxProduct(arr)
+print(maxProduct(arr))
+
