f77bc471ac8e7c1b6c5a31d0db3f953f042c3c9e
csandoval18 <christiansandoval1018@outlook.com>
2023-11-19 23:11:58 -0600

    update

---
+++ CodeNinjas/Binary Search/implement_lower_bound.py
@@ -0,0 +1,38 @@
+# You are given an array 'arr'sorted in non-decreasing order and a number 'x'.
+# You must return the index of lower bound of 'x'.
+# Note:
+# For a sorted array 'arr', 'lower_bound' of a number 'x' is defined as the smallest index 'idx' such that the value 'arr[idx]' is not less than 'x'
+# If all numbers are smaller than 'x', then 'n' should be the 'lower_bound' of 'x', where 'n' is the size of array.
+# Consider 0-based indexing.
+
+# Example:
+# Input: ‘arr’ = [1, 2, 2, 3] and 'x' = 0
+# Output: 0
+# Explanation: Index '0' is the smallest index such that 'arr[0]' is not less than 'x'.
+
+
+# Basically this is a binary search problem since its asking for O(log(n)), but the array has repeating numbers
+
+def lowerBound(arr: [int], n: int, x: int) -> int:
+  l = 0
+  r = n-1
+  
+  while l<=r:
+    m = (l+r) // 2
+    
+    if arr[m] == x:
+      while arr[m] == x:
+        m -= 1
+      return m+1
+    
+    elif arr[m] <= x:
+      l = m+1 
+    elif arr[m] >= x:
+      r = m-1
+  return m
+       
+arr = [1,2,2,3,3,5]
+n = 6
+x = 7
+
+print(lowerBound(arr, n, 7))
