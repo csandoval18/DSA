0707a13af50cd48a46bf5cc2965ff9f7fc74b7a6
csandoval18 <christiansandoval1018@outlook.com>
2023-11-22 11:39:36 -0600

    update

--- CodeNinjas/Binary Search/find_min_rotated_sorted_arr.py
+++ CodeNinjas/Binary Search/find_min_rotated_sorted_arr.py
@@ -1,17 +1,30 @@
 # All ints in nums are unique
 # This means no need to check if l == m == r in rotated arr
 
+# No target given, therefore no need to have base case arr[m] == target 
+
 def findMin(arr: [int]):
   n = len(arr)
   l, r = 0, n-1
+  res = float('inf')
   
   while l<=r:
     m = (l+r)//2
+    
+    if arr[l] < arr[m]:
+      res = min(res, arr[l])
+      l = m + 1
+    elif arr[m] < arr[r]:
+      res = min(res, arr[m])
+      r = m-1
+  return res
+
+nums = [3,4,1,2]
 
 
-
-#  L   M   R
 # [3,4,5,1,2]
+# 3<5 | min = 3 | l=m+1
+# 5<2
 
-#  L   M   R
+#      M L R
 # [3,4,5,1,2]
