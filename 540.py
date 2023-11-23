4c45a9b0239c159282684d5ac263dc5facde93f6
csandoval18 <christiansandoval1018@outlook.com>
2023-11-22 22:06:36 -0600

    update

---
+++ 540.py
@@ -0,0 +1,15 @@
+def singleNonDuplicate(nums: [int]) -> int:
+  n = len(nums)
+  
+  if n == 1:
+    return nums[0]
+  
+  return
+  
+  
+# [1,1,2,3,3,4,4,8.8]
+#  L       M       R
+
+#  0 1 2 3 4 5 6 7 8
+# [1,1,2,3,3,4,4,8,8]
+#  L       M       R
