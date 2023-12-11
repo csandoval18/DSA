a1905573c6deaa2e6b6fa942118ece625bf82ea2
csandoval18 <christiansandoval1018@outlook.com>
2023-12-10 19:18:48 -0600

    update

--- 1011.py
+++ 1011.py
@@ -1,4 +1,10 @@
 # weights = [1,2,3,4,5,6,7,8,9,10]
 # days = 5
+# res = 15
+
+# weights = [1,2,3,4,5,6,7,8,9,10]
+# l = max(weights)
+# r += num for num in weights
+
 def shipWithinDays(weights: [int], days: int) -> int:
-  
+  def calcDaysSum():
