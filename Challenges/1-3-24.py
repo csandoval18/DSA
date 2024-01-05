36638d1431fa2295a106470b0d3d3a2ecfebfffb
csandoval18 <christiansandoval1018@outlook.com>
2024-01-04 19:00:39 -0600

    update

--- Challenges/1-3-24.py
+++
@@ -1,4 +0,0 @@
-# 2870. Minimum Number of Operations to Make Array Empty
-
-def minOperations(nums: [int]) -> int:
-  
---
+++ CodeNinjas/Two Pointers/sum_of_two_els_eqls_third.py
@@ -0,0 +1,23 @@
+def findTriplet(arr: [int], n: int) -> [int]:
+  arr.sort()
+  
+  for i in range(n-1,-1,-1):
+    j, k = 0, i-1
+    
+    while j<k:
+      curr_sum = arr[j] + arr[k]
+      if curr_sum == arr[i]:
+        return [arr[i], arr[j], arr[k]]
+      elif curr_sum < arr[i]:
+        j += 1
+      else:
+        k -= 1
+  return []
+
+
+arr = [10,5,5,6,2]
+# j
+# 10,5,5,6,2
+#        k i
+
+print(findTriplet(arr, len(arr)))
