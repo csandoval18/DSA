071fe3747d764ef976bf99db0e345bee4f4a2eb2
csandoval18 <christiansandoval1018@outlook.com>
2024-01-07 14:33:09 -0600

    update

---
+++ Challenges/01-06-24.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+
+
+def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+  res, n = 0, len(nums)
+  dp = [defaultdict(int) for _ in range(n)]
+
+  for i in range(n):
+    for j in range(i):
+      diff = nums[i] - nums[j]
+      dp[i][diff] += 1+dp[j][diff]
+      res += 1 + dp[j][diff]
+  return res-(n*(n-1)//2)
+
+
+def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+  res, n = 0, len(nums)
+  dp = [defaultdict(int) for _ in range(n)]
+
+  for i in range(n):
+    for j in range(i):
+      diff = nums[i] - nums[j]
+      dp[i][diff] += 1 + dp[j][diff]
+      res += dp[j][diff]
+  return res
