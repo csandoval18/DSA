3e3caaa14242bd5f46068c308ffa2d371d97744a
csandoval18 <christiansandoval1018@outlook.com>
2024-01-08 12:00:13 -0600

    update

--- CodeNinjas/Recursion/frog_jump.py
+++ CodeNinjas/Recursion/frog_jump.py
@@ -1,6 +1,26 @@
 from ast import List
 
 def frogJump(n: int, heights: List[int]) -> int:
+  # Recusive function starting from 0
+  def min_cost_recursive(heights, curr_idx):
+    # Base case: if at the first position, no cost to move
+    if curr_idx == 0:
+      return 0
+
+    # Base case: if at the second position, consider only 1-step move
+    if curr_idx == 1:
+      return abs(heights[curr_idx] - heights[curr_idx - 1])
+
+    # Calculate the cost for taking 1 step
+    one_step_cost = abs(heights[curr_idx] - heights[curr_idx - 1]) + min_cost_recursive(heights, curr_idx - 1)
+    # Calculate the cost for taking 2 steps
+    two_step_cost = abs(heights[curr_idx] - heights[curr_idx - 2]) + min_cost_recursive(heights, curr_idx - 2)
+
+    # Return the minimum of the two costs
+    return min(one_step_cost, two_step_cost)
+    
+    
+  # Recursive function starting from n-1
   def rec(heights, curr_idx):
     # Base case: if at the last position, no cost to move
     if curr_idx == n-1: 
@@ -13,22 +33,24 @@ def frogJump(n: int, heights: List[int]) -> int:
     
     # Calculate the cvost for taking 1 step
     one_step_cost = abs(heights[curr_idx]-heights[curr_idx + 1]) + rec(heights, curr_idx + 1)
-    
     # Calculate the cost for taking 2 steps
     two_step_cost = abs(heights[curr_idx]-heights[curr_idx + 2]) + rec(heights, curr_idx + 2)
-  
+
     # Return the minimun of the two costs
     return min(one_step_cost, two_step_cost)
   
   
+  
+# DP 
 def frogJump(n: int, heights: [int]) -> int:
-  dp=[0]*n
-  dp[0]=0
-  for i in range(1,len(heights)):
-    fs=dp[i-1]+abs(heights[i]-heights[i-1])
-    sc=float("inf")
+  dp = [0]*n
+  dp[0] = 0
+  
+  for i in range(1, n):
+    fs = dp[i-1] + abs(heights[i] - heights[i-1])
+    sc = float("inf")
     if i>1:
-      sc=dp[i-2]+abs(heights[i]-heights[i-2])
+      sc = dp[i-2] + abs(heights[i] - heights[i-2])
 
-    dp[i]=min(sc,fs)
+    dp[i] = min(sc,fs)
   return dp[-1]
