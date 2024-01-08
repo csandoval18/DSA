838d7ac017dcf5539c4bff29e7ce36913ba0cc7c
csandoval18 <christiansandoval1018@outlook.com>
2024-01-07 20:45:14 -0600

    update

---
+++ CodeNinjas/Recursion/frog_jump.py
@@ -0,0 +1,15 @@
+from ast import List
+
+def frogJump(n: int, heights: List[int]) -> int:
+  def rec(heights, curr_idx):
+    # Base case: if at the last position, no cost to move
+    if curr_idx == n-1: 
+      return 0
+    
+    # Base case: if at the second-to-last position, 
+    # consider only 1-step move
+    if urrent
+  
+  
+  
+  min_cost = []
