9bdd46800c2b19095347922b5fb2ad930fba61b0
csandoval18 <christiansandoval1018@outlook.com>
2024-01-09 10:42:31 -0600

    update

--- Challenges/01-08-24.py
+++ Challenges/01-08-24.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+
 class TreeNode:
   def __init__(self, val=0, left=None, right=None) -> None:
     self.val = val
@@ -8,8 +11,13 @@ def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
   if not root:
     return 0
     
-  
   total_sum = 0
   
   if low <= root.val <= high:
+    total_sum += root.val
+  
+  total_sum += self.rangeSumBST(root.left, low, high)
+  total_sum += self.rangeSumBST(root.right, low, high)
+  
+  return total_sum
     
