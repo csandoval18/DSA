de0042345b4798fbfb84c3b34307406ac153a4c0
csandoval18 <christiansandoval1018@outlook.com>
2024-01-24 21:25:34 -0600

    update

--- examples/Recursion/Merge Sort/merge_sort.py
+++ examples/Recursion/Merge Sort/merge_sort.py
@@ -3,49 +3,61 @@
 # [3,1,2,4,5,2,6,4]
 # [3,1,2,4] [5,2,6,4]
 
-def merge_sortArr(arr: [int]) -> [int]:
-  def merge(arr1: [int], arr2: [int]) -> [int]:
-    n, m = len(arr1), len(arr2)
-    i, j = 0,0
-    res = []
-    
-    while i < n and j < m:
-      if arr1[i] <= arr2[j]:
-        res.append(arr1[i])
-        i += 1
-      else:
-        res.append(arr2[j])
-        j += 1
-    
-    while i < n:
+def merge(arr1: [int], arr2: [int]) -> [int]:
+  n, m = len(arr1), len(arr2)
+  i, j = 0,0
+  res = []
+  
+  while i < n and j < m:
+    if arr1[i] <= arr2[j]:
       res.append(arr1[i])
       i += 1
-      
-    while j < m:
+    else:
       res.append(arr2[j])
       j += 1
-    
-    return res
   
-  def merge_sort(arr: [int]) -> [int]:
-    n = len(arr)
-    if n == 1:
-      return arr
+  while i < n:
+    res.append(arr1[i])
+    i += 1
     
-    m = n//2
-    l = arr[:m]
-    r = arr[m:]
-    
-    left = merge_sort(l) 
-    right = merge_sort(r)
-    
-    return merge(left, right)
+  while j < m:
+    res.append(arr2[j])
+    j += 1
+  
+  return res
 
-  return merge_sort(arr)
+def merge_sort(arr: [int]) -> [int]:
+  n = len(arr)
+  if n == 1:
+    print(arr)
+    return arr
+  
+  m = n//2
+  l = arr[:m]
+  r = arr[m:]
+  
+  left = merge_sort(l) 
+  right = merge_sort(r)
+  
+  return merge(left, right)
+
   
 
 arr1 = [2,4]
 arr2 = [1,3,5]
 
 # print(merge(arr1, arr2))
-print(merge_sortArr([1,3,1,9,6,5]))
+print(merge_sort([1,3,1,9,6,5]))
+
+# Same time complexity, but better space complexity of O(n) instead of the O(n log n) using splicing
+def merge_sort_pointers(arr: [int], l: int, r: int):
+  if l == r:
+    return
+  
+  m = (l+r)//2
+  merge_sort_pointers(arr, l, m)
+  merge_sort_pointers(arr, m+1, r)
+  merge(arr, l, m, r)
+  
+  
+  
