ef55d60a5769f3ebccf342bb66efa961b915a156
csandoval18 <christiansandoval1018@outlook.com>
2024-01-29 13:56:02 -0600

    update

---
+++ examples/Recursion/Subsequence/Permutations/permutations_flag_method.py
@@ -0,0 +1,24 @@
+from typing import List
+
+def permute(nums: [int]) -> [[int]]:
+  n = len(nums)
+  res = []
+  ds = []
+  
+  def util(nums: List[int], freq: List[int]):
+    if len(ds) == n:
+      res.append(ds[:])
+      return
+      
+    for i in range(n):
+      if not freq[i]:
+        ds.append(nums[i])
+        freq[i] = 1
+        util(nums, freq)
+        freq[i] = 0
+        ds.pop()
+    
+  freq = [0] * n
+  util(nums, freq)
+  return res
+  
