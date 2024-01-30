48f256fee87b558aac2be3bd1c195b6efaf29cfa
csandoval18 <christiansandoval1018@outlook.com>
2024-01-29 22:58:23 -0600

    update

--- examples/Recursion/Subsequence/Permutations/permutations_swap_method.py
+++ examples/Recursion/Subsequence/Permutations/permutations_swap_method.py
@@ -19,3 +19,48 @@ def permute(nums: [int]) -> [[int]]:
 
 nums = [1,2,3]
 print(permute(nums))
+print()
+
+def permute1(nums: [int]) -> [[int]]:
+  n = len(nums)
+  res = []
+  
+  def backtrack(idx: int):
+    if idx == n-1:
+      res.append(nums[:])
+      return
+    
+    for i in range(idx, n):
+      nums[i], nums[idx] = nums[idx], nums[i]
+      backtrack(idx+1)
+      nums[i], nums[idx] = nums[idx], nums[i]
+    
+  backtrack(0)
+  return res
+
+arr1 = [1,2,3]
+print(permute1(arr1))
+print()
+
+
+# Either n or n-1 work to start backtracking n-1 just ends it quicker and doesn't take the case where idx == n and is out
+# of range of an element 
+def permute2(nums: [int]) -> [[int]]:
+  n = len(nums)
+  res = []
+  
+  def backtrack(idx: int) -> None:
+    if idx == n-1:
+      res.append(nums[:])
+      return
+      
+    for i in range(idx, n):
+      nums[i], nums[idx] = nums[idx], nums[i]
+      backtrack(idx+1)
+      nums[i], nums[idx] = nums[idx], nums[i]
+  
+  backtrack(0)
+  return res
+
+arr = [1,2,3]
+print(permute2(arr))
