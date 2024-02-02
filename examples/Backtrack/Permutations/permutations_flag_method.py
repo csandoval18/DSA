0bc6c9708e6271fbe61a21d799599c4f0317f8d0
csandoval18 <christiansandoval1018@outlook.com>
2024-02-02 10:17:24 -0600

    update

---
+++ examples/Backtrack/Permutations/permutations_flag_method.py
@@ -0,0 +1,50 @@
+from typing import List
+
+def permute(nums: [int]) -> [[int]]:
+  n = len(nums)
+  res = []
+  
+  def backtrack(freq: List[int], ds: [int]):
+    if len(ds) == n:
+      res.append(ds[:])
+      return
+      
+    for i in range(n):
+      if freq[i] == False:
+        ds.append(nums[i])
+        freq[i] = 1
+        backtrack(freq)
+        freq[i] = 0
+        ds.pop()
+    
+  freq = [0] * n
+  backtrack(freq, [])
+  return res
+
+nums = [1,2,3]
+print(permute(nums))
+print()
+
+
+def permuteRepeating(nums):
+  n = len(nums)
+  res = []
+  freq = [0]*n
+  
+  def backtrack(ds, freq):
+    if len(ds) == n:
+      res.append(ds[:])
+      return
+    
+    for i in range(n):
+      ds.append(nums[i])
+      freq[i] = 1
+      backtrack(ds, freq)
+      ds.pop()
+      freq[i] = 0
+  
+  backtrack([], freq)
+  return res
+
+print(permuteRepeating(nums))
+print()
