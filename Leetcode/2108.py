bb39f36257d57f8a1a7dce985c5acee271037aca
csandoval18 <christiansandoval1018@outlook.com>
2024-02-13 12:50:16 -0600

    update

--- Leetcode/2108.py
+++ Leetcode/2108.py
@@ -4,16 +4,14 @@ def firstPalindrome(words: List[str]) -> str:
   def isPalindrome(s: str, idx: int):
     n = len(s)
     
-    if idx == n//2:
+    if idx >= n//2:
       return True
     
     if s[idx] == s[n-1-idx]:
-      isPalindrome(s, idx+1)
+      return isPalindrome(s, idx+1)
     else:
       return False
     
-    return True
-  
   res = ""
   for word in words:
     if isPalindrome(word, 0):
@@ -24,26 +22,29 @@ def firstPalindrome(words: List[str]) -> str:
 
 # words = ["abc","car","ada","racecar","cool"]
 # words = ["umicrszeaswtfmctwvoogehszwdjrwdcgyxxetbzevxrqphubyqbgpfetguyv","vzdzfwyevkvqhmvqssnvpayihawydzcovzmh","drvqqepesvornphmikbimxennygxisdysssmxjmaaecaqiwdgfxitoopigxauoojsebjmbcrymvpnnfomlkg","vvszfvtykdyfiywxgxmjpcawzdaovbujmexggwczovhspkrdsskxzrpgfaspnbncdspktcyfkkshpyojwwlysizd","epdtgfhgninnwqzztwm","dmhynaogcxyaxsghzjwzyqfuwyinstxoqcdkxeobinpqeplw","ruhxgdrzfyqyqmxclfqosyczqapiizxlzgixdxthhrv","dwcblqnxtrwtqmtqenidhxnifdbmdvobwmcvwjxgbyjzgvrqzlskjbfirauguhyyjhlotuckssrkqzppzbqd","gxdq","paesyowqeguvxozbixjqppeagycjx","glstauwugkidegnllapgzbzchckepmhbameuigsiqywbilwolxuwzzjwzouqknhlkbjzejxtponmkqjlojurxnryxyqy","inyhioiwanafuhsprudtkqztoakxhbmqcmibsxlhycbmqrvtfabsncmiymthcxwkwkq","djknenppuleedpptrfjgqfejcoghnxjlvjalxkyvnujgiiwdbtvgqvgsivkzqcrbfcvooyhqmrlacyiozytmampjwpknrj","zzrpjoogwkdmdxdkdwgwugqtmzryrgtelnvydyqewpdzzptqzvffppgnhhcaiqotmyslntlwjajzuzbawidpxjtyxryg","xmegifttkamzbpjqbghgwdrkvtnuwfmjdmwehdqiyvgpxxlbkcvkemjbmhbyeqyfssytuwaeysvgnidhcgpncxdxxzhrkbvyqfrs","wgljaiyhyfdijjgihseciabfcoqfojhswewpkpartzmaaglvdfifpptycyonigwcgyklapzojivgojcrevugspejmwanolg","oceurgzgvvctgydqexhghwcochhmtoxjugreqfdnsshffngchetrwedhinosdhwlgviohpkjowr","dyl","vjxkcihfmnmmz","aheg","dwsomlqmaqifihkwahvywzqamgominhxfsgrgbgvoiopnikhxonpetelfsguvhxgiujrij","pdrjgfqzyqczpwdsfzypgkmsvnpboutmcffqrwuzkchaufymmczrdwulbvbanungpxqk","eudbizaabgfzqytowifsuovmxmkjgajtliyfycbrkxeaarakapqoihawmdzmglfnglpwqnfd","txdclnfwxevu","kslqrafjshhadqszeljcekrpnpxqgppbnadmzmpufvadcghxqdqmafpbnutifigstxyilmgx","yhhvhyoolv","sjtubggxcla","vydf"]
+
 def isPalindrome(s: str, idx: int):
   n = len(s)
   
-  if idx == n//2:
+  if idx >= n//2:
     return True
   
   if s[idx] == s[n-1-idx]:
-    isPalindrome(s, idx+1)
-    
-  return False
-
-  if i == len(s)//2 and s[i] == s[len(s)-1-i]:
-    return True
-    
-  if s[i] != s[len(s)-1-i]:
+    return isPalindrome(s, idx+1)
+  else:
     return False
+
+# def isPalindrome(s: str, idx: int):
+  # if i == len(s)//2 and s[i] == s[len(s)-1-i]:
+  #   return True
+    
+  # if s[i] != s[len(s)-1-i]:
+  #   return False
   
-  return recPalindromev2(s, i+1)
+  # return recPalindromev2(s, i+1)
   
 
 # word = "dwcblqnxtrwtqmtqenidhxnifdbmdvobwmcvwjxgbyjzgvrqzlskjbfirauguhyyjhlotuckssrkqzppzbqd"
 word = "aba"
 print(isPalindrome(word, 0))
+
