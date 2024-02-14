06e9f12322211f5102aea22beb9bb3433c9f9f34
csandoval18 <christiansandoval1018@outlook.com>
2024-02-13 22:23:27 -0600

    update

---
+++ examples/DP/matrix/all_paths.py
@@ -0,0 +1,35 @@
+from typing import List
+
+# Find all paths from (0,0) to (n-1,m-1) in a matrix
+
+# Recursive O(2^n*m)
+def findAllPathsRec(matrix: List[List], i: int, j: int) -> int:
+  if i == 0 and j == 0:
+    return 1
+  
+  if i < 0 or j < 0:
+    return 0
+  
+  up = findAllPathsRec(matrix, i-1, j)
+  left = findAllPathsRec(matrix, i, j-1)
+  
+  return up + left
+
+# Memoize overlapping subproblems
+
+def findAllPathsRec(matrix: List[List], i: int, j: int, dp: List[List[int]]) -> int:
+  if i == 0 and j == 0:
+    return 1
+  
+  if i < 0 or j < 0:
+    return 0
+  
+  if dp[i][j] != -1:
+    return dp[i][j]
+  
+  up = findAllPathsRec(matrix, i-1, j)
+  left = findAllPathsRec(matrix, i, j-1)
+  
+  dp[i][j] = up+left
+  return up + left
+  
