c6ea9ba557049d2244df733d5c595ac90ee85b0c
csandoval18 <christiansandoval1018@outlook.com>
2024-02-16 16:49:36 -0600

    update

--- examples/DP/matrix/min-max-falling-path.py
+++ examples/DP/matrix/min-max-falling-path.py
@@ -4,12 +4,52 @@ def getMaxPathSum(matrix: List[List[int]]) -> int:
   n = len(matrix)
   m = len(matrix[0])
   
-  def rec(i: int, j: int) -> int:
+  def f(i: int, j: int) -> int:
+    if j < 0 or j >= m:
+      return int(-1e9)
+    
+    if i == 0:
+      # We reached the first row starting from row n-1
+      return matrix[0][j]
+    
+    up = matrix[i][j] + f(i-1, j, matrix)
+    ld = matrix[i][j] + f(i-1, j-1, matrix)
+    rd = matrix[i][j] + f(i-1, j, matrix)
+    
+    return max(up, ld, rd)
   
-  curr_max = int(-1e9)
+  res = int(1e9)
+  for j in range(m):
+    res = max(res, f(n-1, j))
   
+  return res
+
+def getMaxPathSum(matrix: List[List[int]]) -> int:
+  n = len(matrix)
+  m = len(matrix[0])
+  
+  def f(i: int, j: int, dp: List[List[int]]) -> int:
+    if j < 0 or j >= m:
+      return int(-1e9)
+    
+    if i == 0:
+      # We reached the first row starting from row n-1
+      return matrix[0][j]
+    
+    if dp[i][j] != -1:
+      return dp[i][j]
+    
+    up = matrix[i][j] + f(i-1, j, matrix)
+    ld = matrix[i][j] + f(i-1, j-1, matrix)
+    rd = matrix[i][j] + f(i-1, j, matrix)
+    
+    dp[i][j] = max(up, ld, rd)
+    return dp[i][j]
+  
+  dp = [[-1 for _ in range(m)] for _ in range(n)]
+  res = int(1e9)
   
   for j in range(m):
-    res = max(res, getMaxPathSum(n-1, j, matrix))
+    res = max(res, f(n-1, j))
   
-  
+  return res
