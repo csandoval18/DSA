7d68fd287547e84e65cd69f262ad655ef1fd82ec
csandoval18 <christiansandoval1018@outlook.com>
2024-02-16 21:01:19 -0600

    update

--- examples/DP/matrix/2_min_path_sum.py
+++ examples/DP/matrix/2_min_path_sum.py
@@ -7,12 +7,17 @@ def minPathSumRec(matrix: List[List[int]]) -> int:
       return matrix[0][0]
     
     if i < 0 or j < 0:
-      return float('inf')
+      return int(1e9)
     
-    up = helper(i-1, j)
-    down = helper(i, j-1)
+    up = matrix[i][j] + helper(i-1, j)
+    left = matrix[i][j] + helper(i, j-1)
     
-    return min(up, down)
+    return min(up, left)
+  
+  n = len(matrix)
+  m = len(matrix[0])
+  res = int(1e9)
+  return helper(n-1, m-1)
 
 # If overlapping subproblems present in recursion tree => Memoization
 def minPathSumMemo(matrix: List[List[int]]) -> int:
@@ -21,27 +26,27 @@ def minPathSumMemo(matrix: List[List[int]]) -> int:
       return matrix[0][0]
     
     if i < 0 or j < 0:
-      return float('inf')
+      return int(1e9)
       
     if dp[i][j] != -1:
       return dp[i][j]
     
-    up = helper(i-1, j)
-    left = helper(i, j-1)
+    up = matrix[i][j] + helper(i-1, j, dp)
+    left = matrix[i][j] + helper(i, j-1, dp)
     
     dp[i][j] = min(up, left)
     return dp[i][j]
   
-  dp = [[-1 for _ in range(m)] for _ in range(n)]
   n = len(matrix)
   m = len(matrix[0])
+  dp = [[-1 for _ in range(m)] for _ in range(n)]
   return helper(n-1, m-1, dp)
 
 # Tabulation 
-def minPathSum(matrix: List[List[int]]) -> int:
+def minPathSumTab(matrix: List[List[int]]) -> int:
   n = len(matrix)
-  m = len(matrix)
-  dp = [[-1 for _ in range(m)] for _ in range(n)]
+  m = len(matrix[0])
+  dp = [[0 for _ in range(m)] for _ in range(n)]
   
   for i in range(n):
     for j in range(m):
@@ -56,7 +61,7 @@ def minPathSum(matrix: List[List[int]]) -> int:
         
         left = matrix[i][j]
         if j > 0:
-          left += matrix[i][j]
+          left += dp[i][j-1]
         else:
           # We have reached the farthest left index, set the left to a big integer
           left += int(1e9)
@@ -66,9 +71,9 @@ def minPathSum(matrix: List[List[int]]) -> int:
   return dp[n-1][m-1]
         
 # Space Optimization
-def minPathSum(matrix: List[List[int]]) -> int:
+def minPathSumSO(matrix: List[List[int]]) -> int:
   n = len(matrix)
-  m = len(matrix)
+  m = len(matrix[0])
   # dp = [[-1 for _ in range(m)] for _ in range(n)]
   
   prevRow = matrix[0]
@@ -112,4 +117,10 @@ def uniquePaths(m: int, n: int) -> int:
     prevRow = newRow
   
   return prevRow[0]
-  
+
+# grid = [[5, 9, 6], [11, 5, 2]]
+grid = [[1,3,1],[1,5,1],[4,2,1]]
+print(minPathSumRec(grid))
+print(minPathSumMemo(grid))
+print(minPathSumTab(grid))
+print(minPathSumSO(grid))
