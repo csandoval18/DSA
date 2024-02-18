debe3d2a69893f982685ec79c5ddbb1c75baf238
csandoval18 <christiansandoval1018@outlook.com>
2024-02-18 12:34:39 -0600

    update

--- examples/DP/practice/all_paths_2.py
+++ examples/DP/practice/all_paths_2.py
@@ -96,38 +96,23 @@ def uniquePathsWithObstacles2(obstacleGrid: List[List[int]]) -> int:
 
 
 def uniquePathsWithObstacles(obstacleGrid: List[List[int]]) -> int:
-  n = len(obstacleGrid)  # Number of rows
-  m = len(obstacleGrid[0])  # Number of columns
+  n, m = len(obstacleGrid), len(obstacleGrid[0])
+  prevRow = [0]*n
+  prevRow[0] = 1 if obstacleGrid[0][0] == 0 else 0 
   
-  prevRow = [0] * m  # Initialize the previous row
-  # Initialize the first element of prevRow based on whether the top-left cell is an obstacle or not
-  prevRow[0] = 1 if obstacleGrid[0][0] == 0 else 0  
-  
-  for i in range(n):
-    currRow = [0] * m  # Initialize the current row
-    for j in range(m):
-      if i == 0 and j == 0:  # Skip the top-left cell
-        continue
-      
-      if obstacleGrid[i][j] == 1:  # If the current cell is an obstacle
-        currRow[j] = 0  # Set the number of paths to 0
+  for i in range(m):
+    currRow = [0]*n
+    currRow[0] = 1 if obstacleGrid[0][0] == 0 else 0
+    
+    for j in range(1, n):
+      if obstacleGrid[i][j] == 0:
+        currRow[j] = currRow[j-1] + prevRow[j] 
       else:
-        # Calculate the number of paths from the top cell if it's not an obstacle
-        top = prevRow[j] if i > 0 else 0
-        # Calculate the number of paths from the left cell if it's not an obstacle
-        left = currRow[j - 1] if j > 0 else 0
+        currRow[j] = 0
+    prevRow = currRow
+  
+  return prevRow[m-1]
         
-        # Update the current cell with the sum of paths from the top and left cells
-        currRow[j] = top + left
-  
-    prevRow = currRow  # Update the previous row for the next iteration
-    print("currRow:", currRow)  # Debugging: Print currRow at each iteration
-  
-  print("prevRow:", prevRow)  # Debugging: Print prevRow after the loop
-  
-  return prevRow[-1]  # Return the count of unique paths to the bottom-right cell
-
-
   
 n = 3
 m = 7
