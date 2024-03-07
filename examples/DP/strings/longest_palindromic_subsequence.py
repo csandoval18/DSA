92dcb31916c302e3ee265780bda4786af247478e
csandoval18 <christiansandoval1018@outlook.com>
2024-03-07 16:34:41 -0600

    update

--- examples/DP/strings/longest_palindromic_subsequence.py
+++ examples/DP/strings/longest_palindromic_subsequence.py
@@ -1,8 +1,11 @@
+from typing import List
+
 # 516. Longest Palindromic Subsequence
   
 # This solution generates all subsequences and searches checks if they are all palindromes 
 # then the longest palindrome found is returned
-def longestPalindromeSubseqVer1Rec(s: str) -> int:
+
+def longestPalindromeSubseqVer1AllSubsets(s: str) -> int:
   def isPalindrome(string: str) -> bool:
     return string == string[::-1]
     
@@ -23,29 +26,55 @@ def longestPalindromeSubseqVer1Rec(s: str) -> int:
   return f(n-1, "")
 
 
-
-# Find lcs of s and s[::-1]
-def longestPalindromeSubseq(self, s: str) -> int:
+# Recursion
+def longestPalindromeSubseqRec(s: str) -> int:
   def lcs(i, j):
-    if i == 0 and j == 0:
+    if i == 0 or j == 0:
       return 0
     
-    if s[i] == t[j]:
-      return lcs(i -1, j-1)
+    if s[i-1] == t[j-1]:
+      return 1 + lcs(i-1, j-1)
       
     return max(lcs(i-1, j), lcs(i, j-1))
   
   t = s[::-1]
   n = len(s)
   return lcs(n, n)
-  
-      
+
+
+# Memoization
+def longestPalindromeSubseqMemo(s: str) -> int:
+  def lcs(i: int, j: int, dp: List[List[int]]):
+    if i == 0 or j == 0:
+      return 0
     
+    if dp[i][j] != -1:
+      return dp[i][j]
+    
+    if s[i-1] == t[j-1]:
+      return 1 + lcs(i -1, j-1, dp)
       
+    return max(lcs(i-1, j, dp), lcs(i, j-1, dp))
   
   
+# Tabulation
+def longestPalindromeSubseqTab(s: str) -> int:
+  n = len(s)
+  p = s[::-1]
+  dp = [[0]*(n+1) for _ in range(n+1)]
+  dp[0][0] = 0
   
+  for i in range(1, n+1):
+    for j in range(1, n+1):
+      if s[i-1] == p[j-1]:
+        dp[i][j] = 1 + dp[i-1][j-1]
+      else:
+        dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+        
+  return dp[n][n]
 
 s = "cbbd"
 # output: 2
-print(longestPalindromeSubseqVer1Rec(s))
+print(longestPalindromeSubseqVer1AllSubsets(s))
+print(longestPalindromeSubseqRec(s))
+print(longestPalindromeSubseqTab(s))
