863800b2f7b9a00e16996fa149bbd45a557cccea
csandoval18 <christiansandoval1018@outlook.com>
2024-03-07 23:13:30 -0600

    update

--- examples/DP/stocks/buy_sell_stock_2.py
+++ examples/DP/stocks/buy_sell_stock_2.py
@@ -11,21 +11,23 @@ def maxProfitMemo(prices: List[int]) -> int:
     
     profit = 0
     if buy:
-      profit = max(-prices[i] + f(i+1, 0), 0 + f(i+1, 1))
+      profit = max(-prices[i] + f(i+1, 0, dp), # Take
+      0 + f(i+1, 1, dp)) # Not take
     else:
-      profit = max(prices[i] + f(i+1, 1), 0 + f(i+1, 0))
+      profit = max(prices[i] + f(i+1, 1, dp), 0 + f(i+1, 0, dp))
     
     return profit
   
   n = len(prices)
-  dp = [[-1]*2 for _ in range(n)]
+  dp = [[-1]*2 for _ in range(n+1)]
   return f(0, 1, dp)
 
-prices = [7,1,5,3,6,4]
-# output: 5
-print(maxProfitMemo(prices))
 
-def maxProfitMemo(prices: List[int]) -> int:
+def maxProfitTab(prices: List[int]) -> int:
   n = len(prices)
   dp = [[0]*2 for _ in range(n+1)]
   dp = [n][0] = dp[n][1] = 0
+  
+prices = [7,1,5,3,6,4]
+# output: 5
+print(maxProfitMemo(prices))
