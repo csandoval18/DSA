6e49aac716ad705cf4f6ea469aee5413613d6911
csandoval18 <christiansandoval1018@outlook.com>
2024-03-15 14:38:35 -0500

    update

--- Algos & DS/Array/Subarrays/930_binary_subarrays_w_sum.py
+++ Algos & DS/Array/Subarrays/930_binary_subarrays_w_sum.py
@@ -13,24 +13,23 @@ def numSubarraysWithSumBF(nums: List[int], goal: int) -> int:
         cnt += 1
   return cnt
 
-def numSubarraysWithSumOP(nums: List[int], goal: int) -> int:
-  n = len(nums)
-  cnt = 0
-  l, r = 0, 0
-  suff = 0
+
+def numSubarraysWithSum1(nums: List[int], goal: int) -> int:
+  hm = {}
+  hm[0] = 1
+  curr_sum = 0
   
-  while l < n:
-    suff += nums[r]
-    
-    if suff == goal:
-      cnt += 1
-    elif suff > goal:
-      l +=  1
-    
-    
-    r += 1
-    
+  for num in nums:
+    curr_sum += num
+    if curr_sum - goal in hm:
+      res += hm[curr_sum - goal]
+    if curr_sum in hm:
+      hm[curr_sum] += 1
+    else:
+      hm[curr_sum] = 1
+      
+  return res
 
 nums = [1,0,1,0,1]
 goal = 2
-print(numSubarraysWithSumOP(nums, goal))
+print(numSubarraysWithSumBF(nums, goal))
