0db0b23f77f865828b4314b4666d17277ad4f22c
csandoval18 <christiansandoval1018@outlook.com>
2024-03-14 20:31:18 -0500

    update

---
+++ Algos & DS/Graphs/BFS.py
@@ -0,0 +1,18 @@
+from ast import List
+import queue
+def BFS(V: int, adj: List[List[int]]) -> List[int]:
+  visited = [[0]*V]
+  q = queue.Queue()
+  bfs = []
+  
+  visited[0] = 1
+  q.put(0)
+  
+  while not q.empty():
+    
+    
+    
+  
+  
+V = 5, E = 4
+adj = {{1,2,3},{},{4},{},{}}
