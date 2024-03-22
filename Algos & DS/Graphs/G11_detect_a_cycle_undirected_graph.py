53e02ed5ade7fded0d9d565b949786c47071fe10
csandoval18 <christiansandoval1018@outlook.com>
2024-03-22 14:54:29 -0500

    update

--- Algos & DS/Graphs/G11_detect_a_cycle_undirected_graph.py
+++ Algos & DS/Graphs/G11_detect_a_cycle_undirected_graph.py
@@ -1,10 +1,11 @@
 from collections import deque
 from typing import List
 
+# TC: O(V+E)
 def detect(src: int, adj: List[int], visited: List[bool]) -> bool:
   visited[src] = 1
   queue = deque()
-  queue.push((src, -1))
+  queue.append((src, -1))
   
   while queue:
     node, parent = queue.popleft()
@@ -12,8 +13,8 @@ def detect(src: int, adj: List[int], visited: List[bool]) -> bool:
     for adjNode in adj[node]:
       if not visited[adjNode]: # If not visited add to queue and mark as visited
         visited[adjNode] = True
-        queue.push(adjNode, node)
-      elif parent != adjNode: # If the adjacent node has been visited, a cycle is detected
+        queue.append((adjNode, node))
+      elif parent != adjNode: # If one of the adjacent nodes is visited but it did not come from the same parent, then there is a cycle
         return True
       
   return False
@@ -21,10 +22,37 @@ def detect(src: int, adj: List[int], visited: List[bool]) -> bool:
         
 def isCycle(V: int, adj: List[List[int]]) -> bool:
   visited = [False] * V
+  
+  # Traverse all node since the input adjacency list might have unconnected components
   for node in range(V):
     if not visited[node]:
       if detect(node, adj, visited):
         return True
   
   return False
+
+
+def isCycle(V: int, adj: List[List[int]]) -> bool:
+  visited = [False] * V
+  
+  def detectCycle(src: int) -> bool:
+    queue = deque()
+    queue.append((src, -1))
     
+    while queue:
+      currNode, parent = queue.popleft
+      
+      for adjNode in adj[currNode]:
+        if not visited[adjNode]:
+          queue.append((adjNode, currNode))
+          visited[adjNode] = True
+        elif parent != adjNode:
+          return True
+    return False
+  
+  
+  for node in range(V):
+    if not visited[node]:
+      if isCycle(node):
+        return True
+  return False
