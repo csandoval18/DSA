4c05d52ac765a2cfb7b32a342c1988d8f5ee0ada
csandoval18 <christiansandoval1018@outlook.com>
2024-03-24 15:14:06 -0500

    update

--- Algos & DS/Graphs/G10_rotten_oranges.py
+++ Algos & DS/Graphs/G10_rotten_oranges.py
@@ -6,6 +6,10 @@ from typing import List
 # since we would need to go through the depth instead of going throw levels 
 # checking top, left, right, down per level
 
+# 2 = Rotten orange
+# 1 = Fresh orange (needs to be decomposed)
+# 0 = Empty cell (can not be visited)
+
 # This solution does not use a visited array to simplify the solution
 def orangesRotting(grid: List[List[int]]) -> List[List[int]]:
   n, m = len(grid), len(grid[0])
@@ -75,6 +79,39 @@ def orangesRottingV2(grid: List[List[int]]) -> List[List[int]]:
         visited[nx][ny] = True # Mark as visited
     
   return -1 if fresh != 0 else minutes
+
+def orangesRottingPrac(grid: List[List[int]]) -> List[List[int]]:
+  n, m = len(grid), len(grid[0])
+  fresh = 0
+  minutes = 0
+  queue = deque()
+  visited = [[False]*m for _ in range(n)]
+  
+  for i in range(n):
+    for j in range(m):
+      if grid[i][j] == 2:
+        queue.append(i, j, 0)
+        visited[i][j] = True
+      elif grid[i][j] == 1:
+        fresh += 1
+  
+  # BFS
+  while queue:
+    x, y, time = queue.popleft()
+    minutes = max(minutes, time)
+    
+    delta_matrix = [(1,0), (-1,0), (0,1), (0,-1)]
+    for dx, dy in delta_matrix:
+      nx = x + dx
+      ny = y + dy
+    
+      if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny] and grid[nx][ny] == 1:
+        queue.append((nx, ny, time+1))
+        visited[nx][ny] = True
+  
+  return -1 if fresh != 0 else minutes
+      
+      
   
 grid = [
   [2,1,1],
--- Algos & DS/Graphs/G16_number_of_distinct_islands.py
+++ Algos & DS/Graphs/G16_number_of_distinct_islands.py
@@ -4,22 +4,26 @@ def countDistinctIslands(grid: List[List[int]]) -> int:
   n, m = len(grid), len(grid[0])
   visited = [[False]*m for _ in range(n)] # visited is a local variable 
   # set<vector<pair<int,int>>> st
-  distinct_islands = set()
+  distinct_islands = set() # We use a set to keep track of the distinct islands
   
+  # DFS traversal to find the total island "land mass" and marking the visited matrix cells
   def dfs(curr_x: int, curr_y: int, island_pattern: List[Tuple[int, int]], ref_x: int, ref_y: int):
-    visited[curr_x][curr_y] = True
-    island_pattern.append((curr_x-ref_x, curr_y-ref_y))
+    visited[curr_x][curr_y] = True # Set current cell as visited
+    island_pattern.append((curr_x-ref_x, curr_y-ref_y)) # Add the current cell coordinates to island pattern list
     
     # delrow = [-1,0,1,0]
     # delcol = [0,-1,0,1]
-    delta_matrix = [(1, 0), (-1,0), (0,1), (0,-1)]
+    delta_matrix = [(1, 0), (-1,0), (0,1), (0,-1)] # Delta matrix to check up, down, left, right from current cell
     for dx, dy in delta_matrix:
-      nrow = curr_x + dx
-      ncol = curr_y + dy
+      nrow = curr_x + dx # New row coordinate calculated
+      ncol = curr_y + dy # New col coordinate calculated
       
+      # Check if the new coordinate of the cell is in range of the matrix, is not already marked as visited, and 
+      # the cell is actually a land cell labeled as '1'
       if 0 <= nrow < n and 0 <= ncol < m and not visited[nrow][ncol] and grid[nrow][ncol] == 1:
-        dfs(nrow, ncol, island_pattern, ref_x, ref_y)
+        dfs(nrow, ncol, island_pattern, ref_x, ref_y) # If the above conditions are met we can continue the dfs traversal
     
+  # This loop traverses the entire 
   for i in range(n):
     for j in range(m):
       if not visited[i][j] and grid[i][j] == 1:
