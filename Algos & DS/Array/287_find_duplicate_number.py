c9977b763b7005de5d1518ed17cb4f0331b5000f
csandoval18 <christiansandoval1018@outlook.com>
2024-03-23 22:09:03 -0500

    update

---
+++ Algos & DS/Array/287_find_duplicate_number.py
@@ -0,0 +1,45 @@
+from typing import List
+
+# 287. Find the Duplicate Number
+
+# Given an array of integers nums containing n + 1 integers 
+# where each integer is in the range [1, n] inclusive.
+
+# There is only one repeated number in nums, return 
+# this repeated number.
+
+# You must solve the problem without modifying the array 
+# nums and uses only constant extra space.
+
+# Brute Force
+
+def findDuplicateBF(nums: List[int]) -> int:
+  n = len(nums)
+  
+  for i in range(n):
+    for j in range(i+1, n):
+      if nums[i] == nums[j]:
+        return nums[i]
+  
+# This can be thought of a cycle detection problem, thus it can be solved
+# efficiently using Floyd's Tortoise and Hare (Cycle Detection) algorithm
+
+def findDuplicate(nums: List[int]) -> int:
+  # Phase 1: Finding the intersection point of the two runners
+  tortoise = nums[0]
+  hare = nums[0]
+  
+  while True:
+    tortoise = nums[tortoise]
+    hare = nums[nums[hare]]
+    
+    if tortoise == hare:
+      break
+    
+    # Phase 2: Finding the "entrance" to the cycle
+    tortoise = nums[0]
+    while tortoise != hare:
+      tortoise = nums[tortoise]
+      hare = nums[hare]
+    
+    return hare
--- Algos & DS/Graphs/G16_number_of_distinct_islands.py
+++ Algos & DS/Graphs/G16_number_of_distinct_islands.py
@@ -3,3 +3,7 @@ from typing import List
 def countDistinctIslands(grid: List[List[int]]) -> int:
   n, m = len(grid), len(grid[0])
   visited = [[False]*m for _ in range(n)]
+  
+  for i in range(n):
+    for j in range(m):
+      
