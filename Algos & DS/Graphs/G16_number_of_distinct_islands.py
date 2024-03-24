89b55a2634350519783034c5b368a0c2b5d05923
csandoval18 <christiansandoval1018@outlook.com>
2024-03-24 14:45:49 -0500

    update

--- Algos & DS/Graphs/G16_number_of_distinct_islands.py
+++ Algos & DS/Graphs/G16_number_of_distinct_islands.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 
 def countDistinctIslands(grid: List[List[int]]) -> int:
   n, m = len(grid), len(grid[0])
@@ -6,29 +6,30 @@ def countDistinctIslands(grid: List[List[int]]) -> int:
   # set<vector<pair<int,int>>> st
   distinct_islands = set()
   
-  def dfs(x1: int, y1: int, island_pattern: List[(int, int)], x2: int, y2: int):
+  def dfs(curr_x: int, curr_y: int, island_pattern: List[Tuple[int, int]], ref_x: int, ref_y: int):
     visited[i][j] = True
-    island_pattern.append((x1-x2, y1-y2))
+    island_pattern.append((curr_x-ref_x, curr_y-ref_y))
     
     # delrow = [-1,0,1,0]
     # delcol = [0,-1,0,1]
     delta_matrix = [(1, 0), (-1,0), (0,1), (0,-1)]
     for dx, dy in delta_matrix:
-      nrow = x1 + dx
-      ncol = x2 + dy
+      nrow = ref_x + dx
+      ncol = ref_y + dy
       
       if 0 < nrow <= n and 0 < ncol <= m and not visited[nrow][ncol] and grid[nrow][ncol] == 1:
-        dfs(nrow, ncol, island_pattern, x2, y2)
+        dfs(nrow, ncol, island_pattern, ref_x, ref_y)
     
   for i in range(n):
     for j in range(m):
       if not visited[i][j] and grid[i][j] == 1:
         island_pattern = []
         dfs(i, j, island_pattern, i, j)
-        distinct_islands.add(island_pattern)
+        distinct_islands.add(tuple(island_pattern))
+  
+  return len(distinct_islands)
       
         
-
 grid = [
   [1, 1, 0, 0, 0],
   [1, 1, 0, 0, 0],
