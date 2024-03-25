6ca6a6ff3ca689d86620da164c3b7d4f80cc7b02
csandoval18 <christiansandoval1018@outlook.com>
2024-03-25 00:29:41 -0500

    update

---
+++ Algos & DS/Graphs/G18_bipartite_graph_dfs.py
@@ -0,0 +1,9 @@
+from typing import List
+
+def isBipartite(V: int, adj: List[List[int]]) -> bool:
+  color = [-1] * V
+  
+  def dfs(start: int, curr_color: int) -> bool:
+    color[start] = 
+  
+  for i
