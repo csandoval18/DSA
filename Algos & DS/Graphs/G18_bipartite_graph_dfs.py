11d17ce316b2fec3f5c4ec2bd3643cb57f8f2acf
csandoval18 <christiansandoval1018@outlook.com>
2024-03-25 13:54:55 -0500

    update

--- Algos & DS/Graphs/G18_bipartite_graph_dfs.py
+++ Algos & DS/Graphs/G18_bipartite_graph_dfs.py
@@ -21,8 +21,23 @@ def isBipartite(V: int, adj: List[List[int]]) -> bool:
         return False
   return True
   
+def isBipartiteLC(graph: List[List[int]]) -> bool:
+  V = len(graph)
+  color = [-1] * V
   
-  #  dfs(1, 0)
-  #   /        \ 
-  # dfs(2, 1)     
-  # dfs()
+  def dfs(node: int, curr_color: int) -> bool:
+    color[node] = curr_color
+    
+    for adjNode in graph[node]:
+      if color[adjNode] == -1:
+        if not dfs(adjNode, 1 - color[node]):
+          return False
+      elif color[adjNode] == color[node]:
+        return False
+    return True
+  
+  for node in range(V):
+    if color[node] == -1:
+      if not dfs(node, 0):
+        return False
+  return True
--- Algos & DS/Recursion & Backtrack/Practice/17_letter_combinations.py
+++ Algos & DS/Recursion & Backtrack/Practice/17_letter_combinations.py
@@ -6,20 +6,41 @@ def letterCombinations(digits: str) -> List[str]:
   n = len(digits)
   if n == 0:
     return ''
+    
   res = []
-
-  def backtrack(path, startindex):
+  def bt(i: int, path: List[str]):
     if len(path) == n:
       res.append(''.join(path))
       return 
     
-    for c in mappings[digits[startindex]]:
+    for c in mappings[digits[i]]:
       path.append(c)
-      backtrack(path, startindex+1)
+      bt(i+1, path)
       path.pop()
 
-  backtrack([], 0)
+  bt(0, [])
   return res
 
+digits = "23"
+print(letterCombinations(digits))
+
             
+def letterCombinations2(digits: str) -> List[str]: 
+  mappings = {'2':'abc', '3':'def','4':'ghi','5':'jkl','6':'mno','7':'pqrs','8':'tuv','9':'wxyz'}
+  n = len(digits)
+  res = []
   
+  def f(i: int, ds: str) -> None:
+    if i == n:
+      res.append(ds)
+      return
+    
+    for char in mappings[digits[i]]:
+      f(i+1, ds+char)
+
+  if digits:
+    f(0, "")
+  return res
+  
+digits = "23"
+print(letterCombinations2(digits))
