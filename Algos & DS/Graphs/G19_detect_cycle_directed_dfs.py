0d48bec7c2bc065ea4962b12dd55e9c1fcd4ec98
csandoval18 <christiansandoval1018@outlook.com>
2024-03-25 21:52:29 -0500

    update

--- Algos & DS/Graphs/G19_detect_cycle_directed_dfs.py
+++ Algos & DS/Graphs/G19_detect_cycle_directed_dfs.py
@@ -8,12 +8,41 @@ def isCyclic(V: int, adj: List[List[int]]) -> bool:
     visited[node] = True
     pathVis[node] = True
     
-    # Traverse for adj nodes
+    for adjNode in adj[node]:
+      if not visited[adjNode]:
+        if dfs(adjNode):
+          return True
+      # If the node has been visited and visited on the same path,
+      # then there is a cycle present
+      elif pathVis[adjNode]:
+        return True
+        
+    pathVis[node] = False
+    return False
     
-  
   for node in range(V):
     if not visited[node]:
       if dfs(node):
         return True
   return False
   
+def isCyclic(V: int, adj: List[List[int]]) -> bool:
+  visited = [False]*V
+  pathVis = [False]*V
+  
+  def dfs(node: int) -> bool:
+    visited[node] = True
+    pathVis[node] = True
+    
+    for adjNode in adj[node]:
+      if not visited[adjNode]:
+        if dfs(adjNode):
+          return True
+      elif pathVis[adjNode]: 
+        return True
+  
+  for node in range(V):
+    if not visited[node]:
+      if dfs(node):
+        return True
+  return False
