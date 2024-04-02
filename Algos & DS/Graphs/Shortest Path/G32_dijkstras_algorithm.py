5e05900dca3daaeb08fffa8ab37a3eb23bddb03c
csandoval18 <christiansandoval1018@outlook.com>
2024-04-01 22:23:13 -0500

    update

---
+++ Algos & DS/Graphs/Shortest Path/G32_dijkstras_algorithm.py
@@ -0,0 +1,8 @@
+# Dijkstra's Algorithm
+
+# This algorithm is used to find the shortest path in a weighted graph.
+# There are 3 methods of implementaition: 
+# 1. Using a Queue
+# 2. Using a Priority Queue
+# 3. Using a Set (fastest method)
+
