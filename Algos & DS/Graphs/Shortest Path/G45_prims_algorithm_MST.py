e9dd2360488014c21298758a53ca359a1aa13613
csandoval18 <christiansandoval1018@outlook.com>
2024-04-04 19:54:39 -0500

    update

---
+++ Algos & DS/Graphs/Shortest Path/G45_prims_algorithm_MST.py
@@ -0,0 +1,22 @@
+from ast import List
+import heapq
+
+def spanningTree(V: int, adj: List[List[int]]):
+  visited = [False]*V
+  pq = [(0,0)]
+  total_sum = 0
+  
+  while pq:
+    wt, node = heapq.heappop(pq)
+    
+    if visited[node]:
+      continue
+      
+    visited[node] = True
+    total_sum += wt
+    
+    for adjNode, edW in adj[node]:
+      if not visited[adjNode]:
+        heapq.heappush(pq, (edW, adjNode))
+    
+  return total_sum
