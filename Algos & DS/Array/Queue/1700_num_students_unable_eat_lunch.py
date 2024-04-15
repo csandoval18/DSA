e6d58f28dda6e729261317b3ee54e58f7caf4cde
csandoval18 <christiansandoval1018@outlook.com>
2024-04-15 15:32:49 -0500

    update

--- Algos & DS/Array/Queue/1700_num_students_unable_eat_lunch.py
+++ Algos & DS/Array/Queue/1700_num_students_unable_eat_lunch.py
@@ -6,8 +6,13 @@ def countStudents(students: List[int], sandwiches: List[int]) -> int:
   
   while student_queue and sandwich_stack:
     if student_queue[0] == sandwich_stack[0]:
-      student_queue.popleft()
-      sandwich_stack.popleft()
+      student_queue.popleft() # Student takes sandwich and leaves queue
+      sandwich_stack.popleft() # sandwich is removed from stack
     else:
       student_queue.append(student_queue.popleft()) # student goes to the end of the line
   
+    # Check if we are in a deadlock: if all remaining students do not want the sandwich at the front
+    if all(s != sandwich_stack[0] for s in student_queue):
+      break
+  
+  return len(student_queue)
