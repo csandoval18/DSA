5a8d7a386369ea84c95b16b58d35b260cac548fa
csandoval18 <christiansandoval1018@outlook.com>
2024-04-15 02:13:16 -0500

    update

--- Algos & DS/Array/Queue/1700_num_students_unable_eat_lunch.py
+++ Algos & DS/Array/Queue/1700_num_students_unable_eat_lunch.py
@@ -1,4 +1,13 @@
 from ast import List
 
 def countStudents(students: List[int], sandwiches: List[int]) -> int:
+  student_queue = deque(students)
+  sandwich_stack = deque(sandwiches)
+  
+  while student_queue and sandwich_stack:
+    if student_queue[0] == sandwich_stack[0]:
+      student_queue.popleft()
+      sandwich_stack.popleft()
+    else:
+      student_queue.append(student_queue.popleft()) # student goes to the end of the line
   
