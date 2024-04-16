d70c29ae62a0ae5e2ca42ebac8e17afba7bb8adf
csandoval18 <christiansandoval1018@outlook.com>
2024-04-16 00:31:23 -0500

    update

--- Algos & DS/Array/Queue/1700_num_students_unable_eat_lunch.py
+++ Algos & DS/Array/Queue/1700_num_students_unable_eat_lunch.py
@@ -1,4 +1,5 @@
 from ast import List
+from collections import deque
 
 def countStudents(students: List[int], sandwiches: List[int]) -> int:
   student_queue = deque(students)
@@ -11,8 +12,15 @@ def countStudents(students: List[int], sandwiches: List[int]) -> int:
     else:
       student_queue.append(student_queue.popleft()) # student goes to the end of the line
   
-    # Check if we are in a deadlock: if all remaining students do not want the sandwich at the front
-    if all(s != sandwich_stack[0] for s in student_queue):
-      break
   
+    # Manual check to see if any student can eat the sandwich at the front
+    found_match = False
+    for student in student_queue:
+      if student == sandwich_stack[0]:
+        found_match = True
+        break
+    
+    if not found_match:
+      break
+      
   return len(student_queue)
