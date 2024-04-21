297c88f7937d6026e332fb1cd9ae1357828ad4ce
csandoval18 <christiansandoval1018@outlook.com>
2024-04-21 18:09:59 -0500

    update

--- Algos & DS/Graphs/practice/1992_find_all_groups_of_farmland.py
+++ Algos & DS/Graphs/practice/1992_find_all_groups_of_farmland.py
@@ -1,3 +1,7 @@
 from typing import List
 
-def findFarmland(self, land: List[List[int]]) -> List[List[int]]:
+def findFarmland(land: List[List[int]]) -> List[List[int]]:
+  n, m = len(land), len(land[0])
+  
+  def dfs(i: int, j: int, coordinates: List[int]):
+    # Check out of bounds and wether the cell is already processed or is not farmland
