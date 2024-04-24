a238f25807b456c1230bfb7dc2dd9b26fe36dbc6
csandoval18 <christiansandoval1018@outlook.com>
2024-04-24 02:24:47 -0500

    update

--- Algos & DS/Graphs/practice/752_open_the_lock_bfs.py
+++ Algos & DS/Graphs/practice/752_open_the_lock_bfs.py
@@ -1,5 +1,20 @@
+from collections import deque
 from typing import List
 
 def openLock(deadends: List[str], target: str) -> int:
   if "0000" in deadends:
     return -1
+  
+  visited = set(deadends) # All deadends are added to visited to avoid them
+  queue = deque()
+  queue.append(("0000", 0)) # Queue holds tuples of (current combination, depth)
+  visited.add("0000") # Mark the start position as visited
+  
+  def neighbors(combination):
+    for i in range(4):
+      digit = int(combination[i])
+      
+      # Produce next and prev digits for the current position
+      for move in (-1, 1):
+        next_digit = (digit + move) % 10
+        
