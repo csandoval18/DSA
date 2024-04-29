06b62805329305b2a59df44f8037f3a593a4cfde
csandoval18 <christiansandoval1018@outlook.com>
2024-04-28 19:16:03 -0500

    update

---
+++ Algos & DS/Graphs/practice/547_number_of_provinces.py
@@ -0,0 +1,13 @@
+from typing import List
+
+def findCircleNum(isConnected: List[List[int]]) -> int:
+  
+
+
+isConnected = [
+#  0 1 2 
+  [1,1,0], # 0 
+  [1,1,0], # 1 
+  [0,0,1]  # 2
+]
+# Output: 2
