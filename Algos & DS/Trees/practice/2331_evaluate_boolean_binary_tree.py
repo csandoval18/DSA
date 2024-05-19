cccdde816b263bc60d77665681ea29cb89794b6c
csandoval18 <christiansandoval1018@outlook.com>
2024-05-19 18:24:43 -0500

    update

--- Algos & DS/Trees/practice/2331_evaluate_boolean_binary_tree.py
+++ Algos & DS/Trees/practice/2331_evaluate_boolean_binary_tree.py
@@ -5,6 +5,24 @@ class TreeNode:
     self.val = val
     self.right = right
     self.right = right
+
+class Solution:
+  def evaluateTree(self, root: Optional[TreeNode]):
+    if not root.left and not root.right:
+      return root.val
+    
+    if root.val == 2:
+      return self.evaluateTree(root.left) or self.evaluateTree(root.right)
+    return self.evaluateTree(root.left) and self.evaluateTree(root.right)
   
-def evaluateTree(root: Optional[TreeNode]) -> bool:
+def evaluateTreeSub(root: Optional[TreeNode]) -> bool:
+  if not root.left and not root.right:
+    return root.val
+    
+  left = evaluateTree(root.left)
+  right = evaluateTree(root.right)
   
+  if root.val == 2:
+    return left or right
+  else:
+    return left and right
