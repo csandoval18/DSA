927e3c146a393c5edf2fd941f7057490894228fd
csandoval18 <christiansandoval1018@outlook.com>
2024-05-19 01:13:41 -0500

    update

---
+++ Algos & DS/Trees/practice/2331_evaluate_boolean_binary_tree.py
@@ -0,0 +1,10 @@
+from typing import Optional
+
+class TreeNode:
+  def __init__(self, val=0, left=None, right=None) -> None:
+    self.val = val
+    self.right = right
+    self.right = right
+  
+def evaluateTree(root: Optional[TreeNode]) -> bool:
+  
