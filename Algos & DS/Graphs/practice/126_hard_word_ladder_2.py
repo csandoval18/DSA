507fa7e74dba6db54a2fcd215cbc0262a5714767
csandoval18 <christiansandoval1018@outlook.com>
2024-05-30 20:08:45 -0500

    update

--- Algos & DS/Graphs/practice/126_hard_word_ladder_2.py
+++ Algos & DS/Graphs/practice/126_hard_word_ladder_2.py
@@ -1,4 +1,23 @@
+from collections import defaultdict, deque
 from typing import List
 
-
 def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
+  wordList = set(wordList) # Convert wordlist to set for faster access
+  if endWord not in wordList:
+    return []
+  
+  # Dictionary to hold all possible transformations
+  hm = defaultdict(list)
+  length = len(beginWord)
+  
+  for word in wordList:
+    for i in range(length):
+      hm[word[:i] + "*" + word[i+1:]].append(word)
+  
+  queue = deque([(beginWord, [beginWord])])
+  visited = set([beginWord])
+  found = False
+  paths = []
+  
+  while queue and not found:
+    
