1ea3e923b6d1e0455a246085c72083664763fce2
csandoval18 <christiansandoval1018@outlook.com>
2024-06-10 22:24:49 -0500

    update

--- Algos & DS/Graphs/practice/743_network_delay_time.py
+++ Algos & DS/Graphs/practice/743_network_delay_time.py
@@ -1,3 +1,30 @@
+from collections import defaultdict
+import heapq
+from typing import List
+
+
+# k = dest
 class Solution:
   def networkDelayTime(times: List[List[int]], n: int, k: int) -> int:
-  
+    adj = defaultdict(list)
+    
+    for u, v, w in times:
+      adj[u].append((v, w))
+    
+    dist = [float('inf')]*n
+    dist[k] = 0
+    
+    pq = [(0, k)]
+    while pq:
+      time, u = heapq.heappop(pq)
+      
+      if time > dist[u]:
+        continue
+    
+    for v, w in adj[u]:
+      new_time = time + w
+      if new_time < dist[v]:
+        heapq.heappush(pq, (new_time, v))
+    
+    max_dist = max(dist)
+    return max_dist if max_dist < float('inf') else -1
