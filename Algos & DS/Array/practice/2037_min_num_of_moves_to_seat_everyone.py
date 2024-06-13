47a0bc5b813a26d3bf0defc171c7572949af0919
csandoval18 <christiansandoval1018@outlook.com>
2024-06-13 10:24:16 -0500

    update

---
+++ Algos & DS/Array/practice/2037_min_num_of_moves_to_seat_everyone.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+  def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
+    
+    
+seats = [3,1,5]
+students = [2,7,4]
--- Challenges/2024/75_sort_colors.py
+++ Challenges/2024/75_sort_colors.py
@@ -1,10 +1,39 @@
 from typing import List
 
 
+# https://leetcode.com/problems/sort-colors/
+
 class Solution:
   def sortColors(self, nums: List[int]) -> None:
+    count0 = 0
+    count1 = 0
+    count2 = 0
     
+    for num in nums:
+      if num == 0:
+        count0 += 1
+      if num == 1:
+        count0 += 1
+      if num == 2:
+        count0 += 1
+    
+    i = 0
+    while i < count0:
+      nums[i] = 0
+      i += 1
+    
+    while i < count0 + count1:
+      nums[i] = 1
+      i += 1
+      
+    while i < count0 + count1 + count2:
+      nums[i] = 1
+      i += 1
+
+    return nums
     
 
 nums = [2,0,2,1,1,0]
-# Output: [0,0,1,1,2,2]https://leetcode.com/problems/sort-colors/
+# Output: [0,0,1,1,2,2]
+s = Solution()
+print(s.sortColors(nums))
