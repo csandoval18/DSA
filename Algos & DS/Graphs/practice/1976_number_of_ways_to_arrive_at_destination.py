a1c00081097a25983d2bd74409137a8a03dfdcb6
csandoval18 <christiansandoval1018@outlook.com>
2024-06-15 15:00:04 -0500

    update

--- Algos & DS/Graphs/practice/1976_number_of_ways_to_arrive_at_destination.py
+++ Algos & DS/Graphs/practice/1976_number_of_ways_to_arrive_at_destination.py
@@ -2,49 +2,13 @@ from collections import defaultdict
 import heapq
 from typing import List
 
+# This is a Dijkstra and DP problem. We use Dijkstra to find the min path, and DP to keep trak of number of ways to reach a node
 
 # u = current node
 # v = adjacent node / neighbor
 # utime = current node weight/time
 # vtime = cost of traversing to adjacent v node
 
-class Solution:
-  def countPaths(self, n: int, roads: List[List[int]]) -> int:
-    MOD = 10**9 + 7
-    
-    # Create adjacency with bidirectional edges
-    adj = defaultdict(list)
-    for u, v, time in roads:
-      adj[u].append((v, time))
-      adj[v].append((u, time))
-      
-    # Dijkstra's algorithm to find the shortest path
-    dist = [float('inf')]*n
-    dist[0] = 0
-    pq = [(0, 0)]
-    ways = {i: 0 for i in range(n)}
-    ways[0] = 1
-    
-    while pq:
-      u, utime = heapq.heappop(pq)
-      
-      # Skip if the popped distance is greater than the recorded shortest distance in dist array
-      if utime > dist[u]:
-        continue
-        
-      for v, vtime in adj[u]:
-        # ntime = utime + vtime
-        if utime + vtime < dist[v]: # Found a shorter parth to the neighbor
-          dist[v] = utime + vtime
-          ways[v] = ways[u]
-          heapq.heappush(pq, (utime + vtime))
-        # Found another shortest path tot he neighbor
-        elif utime == dist[v]:
-          ways[v] = (ways[v] + ways[u]) % MOD
-
-    return ways[n-1]
-    
-    
 class Solution:
   def countPaths(self, n: int, roads: List[List[int]]) -> int:
     MOD = 10**9 + 7
@@ -76,9 +40,39 @@ class Solution:
         if utime + vtime  < dist[v]:
           dist[v] = utime + vtime
           ways[v] = ways[u]  # Reset ways to the new shortest path
-          heapq.heappush(pq, (vtime, v))
+          heapq.heappush(pq, (vtime + utime, v))
         # Found another shortest path to the neighbor
         elif utime + vtime == dist[v]:
           ways[v] = (ways[v] + ways[u]) % MOD
           
+    return ways[n-1]
+
+class Solution:
+  def countPaths(self, n: int, roads: List[List[int]]) -> int:
+    MOD = 10**9+7
+
+    adj = defaultdict(list)
+    for u, v, t in roads:
+      adj[u].append((v, t))
+      adj[v].append((u, t))
+
+    dist = [float('inf')]*n
+    dist[0] = 0
+    ways = [0]*n
+    ways[0] = 1
+    pq = [(0, 0)]
+
+    while pq:
+      ut, u = heapq.heappop(pq)
+
+      if ut > dist[u]:
+        continue
+      
+      for v, vt in adj[u]:
+        if ut + vt < dist[v]:
+          dist[v] = ut + vt
+          ways[v] = ways[u] 
+          heapq.heappush(pq, (ut + vt, v))
+        elif ut + vt == dist[v]:
+          ways[v] = (ways[v] + ways[u]) % MOD
     return ways[n-1]
