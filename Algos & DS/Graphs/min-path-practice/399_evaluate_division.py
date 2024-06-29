6756592c04159d2ba648be1f3b199a039d6ab8d8
csandoval18 <christiansandoval1018@outlook.com>
2024-06-28 19:00:00 -0500

    update

---
+++ Algos & DS/Graphs/min-path-practice/399_evaluate_division.py
@@ -0,0 +1,45 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+  def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+    adj = defaultdict(dict)
+    
+    for (a, b), value in zip(equations, values):
+      adj[a][b] = value
+      adj[b][a] = 1 / value
+    
+    def dfs(start: int, end: int, visited: set):
+      if start not in adj or end not in adj:
+        return -1.0
+      
+      if start == end:
+        return 1.0
+      
+      visited.add(start)
+      for v, val in adj[start].items():
+        if v not in visited:
+          res = dfs(v, end, visited)
+          
+          if res != -1.0:
+            return res * val
+            
+      return -1
+    
+    res = []
+    for a, b in queries:
+      if a in adj and b in adj:
+        res.append(dfs(a, b, set()))
+      else:
+        res.append(-1.0)
+        
+    return res
+    
+# Example usage
+equations = [["a", "b"], ["b", "c"]]
+values = [2.0, 3.0]
+queries = [["a", "c"], ["c", "a"], ["a", "e"], ["a", "a"], ["x", "x"]]
+s = Solution()
+print(s.calcEquation(equations, values, queries))  
+# Output: [6.0, 0.16666666666666666, -1.0, 1.0, -1.0]
--- Algos & DS/Graphs/practice/1334_find_the_city_with_the_smallest_num_of_neighbors_at_threshold_dist.py
+++ Algos & DS/Graphs/practice/1334_find_the_city_with_the_smallest_num_of_neighbors_at_threshold_dist.py
@@ -43,3 +43,40 @@ class Solution:
         res_city = i
     
     return res_city
+    
+class FloydWarshall:
+  def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
+    dist = [[float('inf')] * n for _ in range(n)]
+    for i in range(n):
+      dist[i][i] = 0
+
+    # Fill initial distances from edges
+    for u, v, w in edges:
+      dist[u][v] = w
+      dist[v][u] = w
+    
+    # Floyd-Was
+    for k in range(n):
+      for u in range(n):
+        for v in range(n):
+          if dist[u][v] > dist[u][k] + dist[k][v]:
+            dist[u][v] = dist[u][k] + dist[k][v]
+    
+    # Find the city with the smallest number of neighbors within the distance threshold
+    min_v_cnt = float('inf')
+    res_city = -1
+    
+    for u in range(n):
+      # v_cnt = sum(1 for j in range(n) if dist[u][v] <= distanceThreshold and u != v)
+      v_cnt = 0
+      for v in range(n):
+        if dist[u][v] <= distanceThreshold and u != v:
+          v_cnt += 1
+      
+      if v_cnt < min_v_cnt or (v_cnt == min_v_cnt and u > res_city):
+        min_v_cnt = v_cnt
+        res_city = u
+    
+    return res_city
+
+
