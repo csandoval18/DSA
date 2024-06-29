dfb0ced2b92907cdd6a51b1e70db7eea0d189fa0
csandoval18 <christiansandoval1018@outlook.com>
2024-06-29 12:00:07 -0500

    update

--- Algos & DS/Graphs/min-path-practice/1514_path_w_max_probability.py
+++ Algos & DS/Graphs/min-path-practice/1514_path_w_max_probability.py
@@ -1,5 +1,13 @@
+from collections import defaultdict
 from typing import List
 
 
 class Solution:
   def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
+    adj = defaultdict(list)
+    for (u, v), prob in edges:
+      adj[u].append(v)
+    
+    dist = [float('inf')]*n
+    dist[start_node] = 0
+    pq = [(0, start_node)]
