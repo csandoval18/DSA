0d54f102a26756da27e64bb3705822f5329fdd9a
csandoval18 <christiansandoval1018@outlook.com>
2024-06-30 15:02:21 -0500

    update

--- Algos & DS/Graphs/practice/2192_all_ancestors_of_node_in_dag.py
+++ Algos & DS/Graphs/practice/2192_all_ancestors_of_node_in_dag.py
@@ -3,3 +3,11 @@ from typing import List
 
 class Solution:
   def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
+    # create adj list
+    
+    
+    
+    ans = []
+    
+    # return list answer, where answer[i] is the list of ancestors of ith node sorted in ascending order
+    return ans
