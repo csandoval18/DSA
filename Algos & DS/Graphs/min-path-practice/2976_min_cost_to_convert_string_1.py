fd0b84e02b562647e105d27182a27e9c96b5d8bc
csandoval18 <christiansandoval1018@outlook.com>
2024-06-29 20:52:50 -0500

    update

--- Algos & DS/Graphs/min-path-practice/2976_min_cost_to_convert_string_1.py
+++ Algos & DS/Graphs/min-path-practice/2976_min_cost_to_convert_string_1.py
@@ -1,8 +1,50 @@
+from collections import defaultdict
+import heapq
 from typing import List
 
 
 class Solution:
   def minimumCost(self, source: str, target: str, original: List[str], changed: List[str], cost: List[int]) -> int:
+    def dijkstra(s_char: str, t_char: str) -> int:
+      n = len(source) # same as target len
+      
+      # (original, changed, cost)
+      adj = defaultdict(list)
+      for u, v, swap_cost in zip(original, changed, cost):
+        adj[u].append((v, swap_cost))
+      
+      pq = [(0, s_char)]
+      min_cost = {char: float('inf') for char in adj}
+      min_cost[source] = 0
+      
+      while pq:
+        ucost, u = heapq.heappop(pq)
+        
+        if u == t_char:
+          return ucost
+          
+        if ucost > min_cost[u]:
+          continue
+        
+        for v, vcost in adj[u]:
+          ncost =  ucost + vcost
+          if ncost < min_cost[v]:
+            min_cost[v] = ncost
+            heapq.heappush(pq, (ncost, v))
+            
+      return float('inf')
+    
+    total_min_cost = 0
+    for s_char, t_char in zip(source, target):
+      if s_char != t_char:
+        conversion_cost = dijkstra(s_char, t_char) 
+        if conversion_cost == float('inf'):
+          return -1
+        total_min_cost += conversion_cost
+    
+    return total_min_cost
+          
+      
     
     
 source = "abcd"
@@ -10,3 +52,5 @@ target = "acbe"
 original = ["a","b","c","c","e","d"]
 changed = ["b","c","b","e","b","e"]
 cost = [2,5,5,1,2,20]
+s = Solution()
+print(s.minimumCost(source, target, original, changed, cost))
