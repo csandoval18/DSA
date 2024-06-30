5564c3a1467aff57e3e9b16d1262a83cdff09e18
csandoval18 <christiansandoval1018@outlook.com>
2024-06-30 02:01:22 -0500

    update

--- Algos & DS/Graphs/min-path-practice/2976_min_cost_to_convert_string_1.py
+++ Algos & DS/Graphs/min-path-practice/2976_min_cost_to_convert_string_1.py
@@ -4,49 +4,41 @@ from typing import List
 
 
 class Solution:
+  def find(self, graph, x, y, d):
+    if (x, y) in d:
+      return d[(x, y)]
+    pq, visit = [], set()
+    heapq.heappush(pq, (0, x))
+    
+    while pq:
+      cost, node = heapq.heappop(pq)
+      if node == y:
+        return cost
+      if node in visit:
+        continue
+      visit.add(node)
+      for nei, new_cost in graph[node]:
+        heapq.heappush(pq, (cost+new_cost, nei))                  
+    return -1
+          
+  
   def minimumCost(self, source: str, target: str, original: List[str], changed: List[str], cost: List[int]) -> int:
-    def dijkstra(s_char: str, t_char: str) -> int:
-      n = len(source) # same as target len
-      
-      # (original, changed, cost)
+      n = len(source)
       adj = defaultdict(list)
-      for u, v, swap_cost in zip(original, changed, cost):
-        adj[u].append((v, swap_cost))
+      for o, c, co in zip(original, changed, cost):
+        adj[o].append([c, co])
       
-      pq = [(0, s_char)]
-      min_cost = {char: float('inf') for char in adj}
-      min_cost[source] = 0
+      res, d = 0, {}
+      for i in range(n):
+          val = self.find(adj, source[i], target[i], d)
+          if val == -1:
+              return -1
+          else:
+              res += val
+          d[(source[i], target[i])] = val
       
-      while pq:
-        ucost, u = heapq.heappop(pq)
-        
-        if u == t_char:
-          return ucost
-          
-        if ucost > min_cost[u]:
-          continue
-        
-        for v, vcost in adj[u]:
-          ncost =  ucost + vcost
-          if ncost < min_cost[v]:
-            min_cost[v] = ncost
-            heapq.heappush(pq, (ncost, v))
-            
-      return float('inf')
-    
-    total_min_cost = 0
-    for s_char, t_char in zip(source, target):
-      if s_char != t_char:
-        conversion_cost = dijkstra(s_char, t_char) 
-        if conversion_cost == float('inf'):
-          return -1
-        total_min_cost += conversion_cost
-    
-    return total_min_cost
-          
+      return res
       
-    
-    
 source = "abcd"
 target = "acbe"
 original = ["a","b","c","c","e","d"]
