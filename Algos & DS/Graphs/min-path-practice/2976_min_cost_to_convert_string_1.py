e407d82e57246fb78fbdd44deb680d0e1ec7df85
csandoval18 <christiansandoval1018@outlook.com>
2024-06-30 13:36:59 -0500

    update

--- Algos & DS/Graphs/min-path-practice/2976_min_cost_to_convert_string_1.py
+++ Algos & DS/Graphs/min-path-practice/2976_min_cost_to_convert_string_1.py
@@ -4,40 +4,76 @@ from typing import List
 
 
 class Solution:
-  def find(self, graph, x, y, d):
-    if (x, y) in d:
-      return d[(x, y)]
-    pq, visit = [], set()
-    heapq.heappush(pq, (0, x))
-    
-    while pq:
-      cost, node = heapq.heappop(pq)
-      if node == y:
-        return cost
-      if node in visit:
-        continue
-      visit.add(node)
-      for nei, new_cost in graph[node]:
-        heapq.heappush(pq, (cost+new_cost, nei))                  
-    return -1
-          
-  
   def minimumCost(self, source: str, target: str, original: List[str], changed: List[str], cost: List[int]) -> int:
-      n = len(source)
-      adj = defaultdict(list)
-      for o, c, co in zip(original, changed, cost):
-        adj[o].append([c, co])
+    def dijkstra(x, y, hm):
+      if (x, y) in hm:
+        return hm[(x, y)]
+        
+      pq, visited = [], set()
+      heapq.heappush(pq, (0, x))
       
-      res, d = 0, {}
-      for i in range(n):
-          val = self.find(adj, source[i], target[i], d)
-          if val == -1:
-              return -1
-          else:
-              res += val
-          d[(source[i], target[i])] = val
+      while pq:
+        ucost, u = heapq.heappop(pq)
+        
+        if u == y:
+          return ucost
+        if u in visited:
+          continue
+        visited.add(u)
+        
+        for v, vcost in adj[u]:
+          ncost = ucost + vcost
+          heapq.heappush(pq, (ncost, v))                  
+      return -1
+    
+  
+    n = len(source)
+    adj = defaultdict(list)
+    for o, c, co in zip(original, changed, cost):
+      adj[o].append([c, co])
+    
+    res = 0  
+    hm = {}
+    
+    for i in range(n):
+      val = dijkstra(source[i], target[i], hm)
+      if val == -1:
+        return -1
+      else:
+        res += val
+      hm[(source[i], target[i])] = val
+    
+    return res
+
+class Solution:
+  def minimumCost(self, source: str, target: str, original: List[str], changed: List[str], cost: List[int]) -> int:
+    adj = defaultdict(list)  
+    for u, v, swap_cost in zip(original, changed, cost):
+      adj[u].append((swap_cost, v))
+    
+    lc_chars = [chr(ord('a') + i) for i in range(26)]
+    dist = defaultdict(lambda: float("inf"))
+    
+    for s in lc_chars:
+      pq = [(0, s)]
+      dist[(s, s)] = 0
       
-      return res
+      while pq:
+        distance, vertex = heapq.heappop(pq)
+        
+        if dist[(s, vertex)] != distance:  # Outdated value
+          continue
+        
+        for weight, neighbour in adj[vertex]:
+          if distance + weight < dist[(s, neighbour)]:
+            dist[(s, neighbour)] = distance + weight
+            heapq.heappush(pq, (distance + weight, neighbour))
+        
+    min_cost = 0
+    for i in range(len(source)):
+      min_cost += dist[(source[i], target[i])]
+    
+    return min_cost if min_cost != float("inf") else -1
       
 source = "abcd"
 target = "acbe"
---
+++ Algos & DS/Graphs/practice/2192_all_ancestors_of_node_in_dag.py
@@ -0,0 +1,5 @@
+from typing import List
+
+
+class Solution:
+  def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
