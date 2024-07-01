86eea8c1f12566926857346679f388724ab39a4e
csandoval18 <christiansandoval1018@outlook.com>
2024-07-01 00:12:51 -0500

    update

---
+++ Algos & DS/Array/practice/1550_three_consecutive_odds.py
@@ -0,0 +1,6 @@
+from typing import List
+
+
+class Solution:
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+      n = len(arr)
--- Algos & DS/Graphs/practice/2192_all_ancestors_of_node_in_dag.py
+++ Algos & DS/Graphs/practice/2192_all_ancestors_of_node_in_dag.py
@@ -1,8 +1,8 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 from typing import List
 
 
-class Solution:
+class SolutionDFS:
   def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
     rev_adj = defaultdict(list)
     for u, v in edges:
@@ -12,15 +12,69 @@ class Solution:
     
     def dfs(start: int, u: int):
       for v in rev_adj[u]:
-        if v not in ancestors[u]:
-          ancestors[u].add(v)
-          dfs(node)
+        if v not in ancestors[start]:
+          ancestors[start].add(v)
+          dfs(start, v)
     
     for node in range(n):
-      dfs(node)
+      dfs(node, node)
     
     res = [sorted(list(anc)) for anc in ancestors]
     return res
+
+class SolutionBFS:
+  def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
+    
+    rev_adj = defaultdict(list) 
+    for u, v in edges:
+      rev_adj[v].append[u]
+    
+    ancestors = [set() for _ in range(n)]
+    
+    for node in range(n):
+     q = deque([node]) 
+     vis = set()
+     
+     while q:
+      u = q.popleft()
+      
+      for v in rev_adj[u]:
+        if v not in vis:
+          vis.add(v)
+          ancestors[u].add(v)
+          q.append(v)
+    
+    result = [sorted(list(ancestor_set)) for ancestor_set in ancestors]
+    return result          
+          
+
+class Solution:
+  def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
+    adj = [[] for _ in range(n)]
+
+    for u, v in edges:
+      adj[v].append(u)
+
+    ancestors = [None] * n
+
+    def dfs(node):
+      if ancestors[node] is not None:
+        return ancestors[node]
+
+      result = set()
+
+      for parent in adj[node]:
+        result.add(parent)
+        result.update(dfs(parent))
+
+      ancestors[node] = sorted(result)
+      return ancestors[node]
+
+    for node in range(n):
+      dfs(node)
+
+    return ancestors
+    
 
 n = 8
 edgeList = [[0,3],[0,4],[1,3],[2,4],[2,7],[3,5],[3,6],[3,7],[4,6]]
