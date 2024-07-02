6c368bff56c561c379aa55494784756b25bad474
csandoval18 <christiansandoval1018@outlook.com>
2024-07-02 16:41:05 -0500

    update

--- CodeNinjas/Two Pointers/intersection_2_arr_ll.py
+++ CodeNinjas/Two Pointers/intersection_2_arr_ll.py
@@ -1,14 +1,35 @@
-def intections(arr1: [int], n: int, arr2: [int], m: int):
-  st = set()
-  res = []
+from typing import List
+
+
+class SolutionHM:
+  def intections(self, arr1: List[int], n: int, arr2: List[int], m: int):
+    st = set()
+    res = []
+    
+    for num in arr2:
+      if num not in st:
+        st.add(num)
+    
+    for num in arr1:
+      if num in st:
+        res.append(num)
+        st.remove(num)
+    
+    return res
   
-  for num in arr2:
-    if num not in st:
-      st.add(num)
-  
-  for num in arr1:
-    if num in st:
-      res.append(num)
-      st.remove(num)
-  
-  return res
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        intersection = []
+        nums1.sort()
+        nums2.sort()
+        i = 0
+        for num in nums1:
+            if i >= len(nums2):
+                break
+            while i < len(nums2) - 1 and nums2[i] < num:
+                i += 1
+            if nums2[i] == num:
+                i += 1
+                intersection.append(num)
+
+        return intersection
