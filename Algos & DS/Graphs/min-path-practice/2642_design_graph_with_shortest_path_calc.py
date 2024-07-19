236ae897123f16718f59aa6d630db8f841cd57e9
csandoval18 <christiansandoval1018@outlook.com>
2024-07-19 00:14:02 -0500

    update

---
+++ Algos & DS/Graphs/min-path-practice/2642_design_graph_with_shortest_path_calc.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Graph:
+    def __init__(self, n: int, edges: List[List[int]]):
+      
+    def addEdge(self, edge: List[int]) -> None:
+
+    def shortestPath(self, node1: int, node2: int) -> int:
+        
