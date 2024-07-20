c06567871567170a0dfa37c60bf0ce07a8157409
csandoval18 <christiansandoval1018@outlook.com>
2024-07-20 00:05:24 -0500

    update

--- Algos & DS/Graphs/min-path-practice/2642_design_graph_with_shortest_path_calc.py
+++ Algos & DS/Graphs/min-path-practice/2642_design_graph_with_shortest_path_calc.py
@@ -1,10 +1,37 @@
+import heapq
 from typing import List
 
 
 class Graph:
     def __init__(self, n: int, edges: List[List[int]]):
+        self.adj = [[] for _ in range(n)]
+        self.n = n
+        
+        for u, v, c in edges:
+            self.adj[u].append((v, c))
       
     def addEdge(self, edge: List[int]) -> None:
+        for u, v, c in edge:
+            self.adj[u].append((v, c))
+            
 
     def shortestPath(self, node1: int, node2: int) -> int:
+        dist = [float('inf')] * self.n 
+        pq = [(node1, 0)]
         
+        while pq:
+            uc,  u = heapq.heappop(pq)
+            
+            if uc > dist[u]:
+                continue
+                
+            for v, vc in self.adj[u]:
+                nc = uc + vc
+                
+                if nc < dist[v]:
+                    dist[v] = nc
+                    
+        if dist[node1] != float('inf'):
+            return dist[node1]
+        return -1
+                    
