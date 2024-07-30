a8ae5a61ef8d6efd5cc48929982ecb812f10ae7a
csandoval18 <christiansandoval1018@outlook.com>
2024-07-29 19:13:34 -0500

    update

---
+++ Algos & DS/DP/practice/1395_count_num_teams.py
@@ -0,0 +1,13 @@
+from typing import List
+
+# There are n soldiers standing in a line. Each soldier is assigned a unique rating value.
+# You have to form a team of 3 soldiers amongst them under the following rules:
+
+# - Choose 3 soldiers with index (i, j, k) with rating (rating[i], rating[j], rating[k]).
+# - A team is valid if: (rating[i] < rating[j] < rating[k]) or (rating[i] > rating[j] > rating[k]) where (0 <= i < j < k < n).
+
+# Return the number of teams you can form given the 
+
+class Solution:
+  def numTeams(self, rating: List[int]) -> int:
+    
---
+++ Algos & DS/Graphs/shortest-path-practice/hard/3123_find_edges_in_shortest_path.py
@@ -0,0 +1,2 @@
+class Solution:
+  def findAnswer(self, n: int, edges: List[List[int]]) -> List[bool]:
