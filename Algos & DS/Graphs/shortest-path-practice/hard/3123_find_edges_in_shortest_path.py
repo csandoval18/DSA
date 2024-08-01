258e9798facdfe0c5b678227b441f25d7148783e
csandoval18 <christiansandoval1018@outlook.com>
2024-08-01 17:59:58 -0500

    update

--- Algos & DS/Graphs/shortest-path-practice/hard/3123_find_edges_in_shortest_path.py
+++ Algos & DS/Graphs/shortest-path-practice/hard/3123_find_edges_in_shortest_path.py
@@ -5,8 +5,6 @@ from typing import List
 # The graph consists of m edges represented by a 2D array edges, where edges[i] = [a, b, w] 
 # indicates that there is an edge between nodes
 
-
-
 class Solution:
   def findAnswer(self, n: int, edges: List[List[int]]) -> List[bool]:
     adj = [[] for _ in range(n)]
@@ -15,21 +13,32 @@ class Solution:
       adj[u].append((v, w))
       adj[v].append((u, w))
       
-    dist = [float('inf')]*n
-    dist[n-1] = 0
-    pq = [(0, n-1)]
-    res = [False]*n
-    
-    while pq:
-      uw, u = heappop(pq)
+    def dijkstra(start: int) -> List[int]:
+      dist = [float('inf')]*n
+      dist[start] = 0
+      pq = [(0, start)]
       
-      if uw > dist[u]:
-        continue
+      while pq:
+        uw, u = heappop(pq)
         
-      for v, vw in adj[u]:
-        nw = uw + vw
-        
-        if nw < dist[v]:
-          dist[v] = nw
-          heappush(pq, (nw, v))
+        if uw > dist[u]:
+          continue
+          
+        for v, vw in adj[u]:
+          nw = uw + vw
+          
+          if nw < dist[v]:
+            dist[v] = nw
+            heappush(pq, (nw, v))
+            
+      return dist
+      
+    dist_0 = dijkstra(0)
+    dist_n1 = dijkstra(n-1)
+    total_dist = dist_0[n-1]
     
+    res = []
+    for u, v, w in edges:
+      res.append(dist_0[u] + w + dist_n1[v] == total_dist)
+    
+    return res
