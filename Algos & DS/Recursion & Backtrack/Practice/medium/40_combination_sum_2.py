296eb8e5d15a8fd270766efb99188d8008b22ff8
csandoval18 <christiansandoval1018@outlook.com>
2024-08-13 00:36:07 -0500

    update

--- Algos & DS/Recursion & Backtrack/Practice/medium/40_combination_sum_2.py
+++ Algos & DS/Recursion & Backtrack/Practice/medium/40_combination_sum_2.py
@@ -59,3 +59,24 @@ candidates = [10, 1, 2, 7, 6, 1, 5]
 target = 8
 s = Solution()
 print(s.combinationSum2(candidates, target))
+
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+         candidates.sort()
+         res = []
+
+         def dfs(target, start, comb):
+             if target < 0:
+                 return
+             if target == 0:
+                 res.append(comb)
+                 return
+             for i in range(start, len(candidates)):
+                 if i > start and candidates[i] == candidates[i-1]:
+                     continue
+                 if candidates[i] > target:
+                     break
+                 dfs(target-candidates[i], i+1, comb+[candidates[i]])
+
+         dfs(target, 0, [])
+         return res
