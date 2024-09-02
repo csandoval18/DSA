9e120d13faa0d515d41e691ea83564410a82b010
csandoval18 <christiansandoval1018@outlook.com>
2024-09-02 18:51:48 -0500

    update

--- Algos & DS/DP/practice/easy/118_pascals_triangle.py
+++ Algos & DS/DP/practice/easy/118_pascals_triangle.py
@@ -1,12 +1,23 @@
 from typing import List
 
+# [1]
+# [1,1]
+# [1,2,1]
+# [1,3,3,1]
+# [1,4,6,4,1]
+
 
 class Solution:
   def generate(self, numRows: int) -> List[List[int]]:
-    res = []
+    triangle = [] # Empty triangle to store the rows of Pascal's Triangle
     
-    for i in range(numRows):
-      row = [1] * (i+1) # Initializ each row with 1s
+    # A loop is started that will iterate 'numRows' times.
+    # Each iteration represents the construction of a new row in Pascal's Triangle.
+    for i in range(numRows): 
+      # A new row is initialized with '1's. The number of 1's is equal to (i+1),
+      # where
+      row = [1] * (i+1) 
       
       for j in range(1, i): # Calculate the values for the current row
-        row [j] = res[i-1][j-1] + res[i-1][j]
+        row [j] = triangle[i-1][j-1] + triangle[i-1][j]
+        
