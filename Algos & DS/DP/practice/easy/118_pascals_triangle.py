530d592c6daa4789958bf3bb183684063213372b
csandoval18 <christiansandoval1018@outlook.com>
2024-09-04 20:27:21 -0500

    update

--- Algos & DS/DP/practice/easy/118_pascals_triangle.py
+++ Algos & DS/DP/practice/easy/118_pascals_triangle.py
@@ -6,7 +6,6 @@ from typing import List
 # [1,3,3,1]
 # [1,4,6,4,1]
 
-
 class Solution:
   def generate(self, numRows: int) -> List[List[int]]:
     triangle = [] # Empty triangle to store the rows of Pascal's Triangle
@@ -18,6 +17,19 @@ class Solution:
       # where
       row = [1] * (i+1) 
       
-      for j in range(1, i): # Calculate the values for the current row
-        row [j] = triangle[i-1][j-1] + triangle[i-1][j]
-        
+      for j in range(1, i): # Calculate the middle values for the current row
+        row[j] = triangle[i-1][j-1] + triangle[i-1][j]
+      triangle.append(row)
+    return triangle
+    
+class Solution1:
+  def generate(self, numRows: int) -> List[List[int]]:
+    triangle = []
+    
+    for x in range(numRows):
+      row = [1] * (x+1)
+      
+      for y in range(1, y):
+        row[y] = triangle[x-1][y-1] + triangle[x-1][y]
+      triangle.append(row)
+    return triangle
