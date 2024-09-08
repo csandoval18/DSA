37ef461c1252c00d9582a1af8052db4d0d08d26d
csandoval18 <christiansandoval1018@outlook.com>
2024-09-08 16:47:11 -0500

    update

--- Algos & DS/DP/practice/easy/392_is_subsequence.py
+++ Algos & DS/DP/practice/easy/392_is_subsequence.py
@@ -41,3 +41,18 @@ class Solution:
       if j == len(s):
         return True
     return False
+
+class Solution:
+  def isSubsequence(self, s: str, t: str) -> bool:
+    i = 0
+    j = 0
+    if len(s) > len(t):
+      return False
+    
+
+    while i < len(s) and j < len(t):
+      if s[i] == t[j]:
+        i += 1
+      j += 1
+          
+    return i == len(s)
