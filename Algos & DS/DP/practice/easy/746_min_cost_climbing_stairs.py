39b8ec2c7c2fa972845e665448900c01893fcfc8
csandoval18 <christiansandoval1018@outlook.com>
2024-09-10 14:32:18 -0500

    update

--- Algos & DS/DP/practice/easy/746_min_cost_climbing_stairs.py
+++ Algos & DS/DP/practice/easy/746_min_cost_climbing_stairs.py
@@ -17,19 +17,32 @@ class Solution:
             second = curr
             
         return min(first, second)
+    
+class SolutionRecursion:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+        dp = [-1] * (n+1)
+        
+        def rec(i: int):
+            if i <= 1: # Base case: if you're at or before step 0, no cost to pay
+                return 0 
+        
+    
 
 class SolutionRecursion:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         n = len(cost)
         
-        def minCost(i):
+        def rec(i: int):
             # Base case: if you're at or beyond the last step, no cost to pay anymore
             if i >= n:
                 return 0
             # Recursive case: take the current step and move to the next step
-            return cost[i] + min(minCost(i+1), minCost(i+2))
+            return cost[i] + min(rec(i+1), rec(i+2))
         # You can start from either step 0 or step 1
-        return min(minCost(0), minCost(1))
+        return min(rec(0), rec(1))
         
 cost = [10,15,20]
 # Output: 15
+s = SolutionRecursion()
+print(s.minCostClimbingStairs(cost))
