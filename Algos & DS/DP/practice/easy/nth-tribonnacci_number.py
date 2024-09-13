b293349e19e0b5bbb64b342545e646ed80d2640c
csandoval18 <christiansandoval1018@outlook.com>
2024-09-13 14:53:55 -0500

    update

---
+++ Algos & DS/DP/practice/easy/nth-tribonnacci_number.py
@@ -0,0 +1,36 @@
+class SolutionRecursion:
+  def tribonacci(self, n: int) -> int:
+    if n == 0:
+      return 0
+    if n == 1 or n == 2:
+      return 1
+    return self.tribonacci(n-1) + self.tribonacci(n-2) + self.tribonacci(n-3)
+    
+class SolutionRecursion:
+  def tribonacci(self, n: int) -> int:
+    if n == 0:
+      return 0
+    if n == 1 or n == 2:
+      return 1
+    return self.tribonacci(n-1) + self.tribonacci(n-2) + self.tribonacci(n-3)
+
+class SolutionTabulation:
+  def tribonacci(self, n: int) -> int:
+    if n == 0:
+      return 0
+    if n == 1 or n == 2:
+      return 1
+      
+    dp = [-1]*(n+1)
+    dp[0] = 0
+    dp[1] = 1
+    dp[2] = 1
+    
+    for i in range(3, n+1):
+      dp[i] = dp[i-3] + dp[i-2] + dp[i-1]
+    
+    return dp[n]
+  
+n = 4
+s = SolutionTabulation()
+print(s.tribonacci(n))
