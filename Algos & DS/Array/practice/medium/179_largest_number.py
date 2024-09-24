a23c7416d5cd8e9259da473b646dcff77ab63fbf
csandoval18 <christiansandoval1018@outlook.com>
2024-09-24 13:31:05 -0500

    update

--- Algos & DS/Array/practice/medium/179_largest_number.py
+++ Algos & DS/Array/practice/medium/179_largest_number.py
@@ -1,6 +1,10 @@
 from functools import cmp_to_key
 from typing import List
 
+# This approach works because lexicographical comparison directly reflects
+# how string concatenation will behave. When you compare two numbers as strings, the 
+# lexicographical ordering tells you which combination of the two strings will yield a 
+# larger number when placed next to each other.
 
 class Solution:
   def largestNumber(self, nums: List[int]) -> str:
@@ -8,7 +12,7 @@ class Solution:
     nums_str = list(map(str, nums))
     
     # Define custom comparator
-    def compare(x: int, y: int):
+    def compare(x: str, y: str):
       if x + y > y + x:
         return -1
       else:
@@ -20,3 +24,10 @@ class Solution:
     res = ''.join(nums_str)
     # Handle case where the result might be all zeroes
     return '0' if res[0] == '0' else res
+    
+nums = [9, 34, 5, 30]
+strings = [9, 34, 5, 30]
+
+# Compare "9" and "34":
+# - x + y = "934"
+
