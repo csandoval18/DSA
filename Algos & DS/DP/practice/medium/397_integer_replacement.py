fd1128c2d564d66acee5bc679baf0220007810e3
csandoval18 <christiansandoval1018@outlook.com>
2024-11-13 18:01:34 -0600

    update

--- Algos & DS/DP/practice/medium/397_integer_replacement.py
+++ Algos & DS/DP/practice/medium/397_integer_replacement.py
@@ -5,6 +5,51 @@ Given a positive integer n, you can apply one of the following operations:
 
 Return the minimum number of operations needed for n to become 1.
 '''
-class Solution:
+
+class SolutionRec:
   def integerReplacement(self, n: int) -> int:
+    def helper(n: int) -> int:
+      if n <= 1:
+        return 0
+        
+      op = 0
+      if n % 2 == 0:
+        op += 1 + helper(n // 2)
+      else:
+        op += 1 + min(helper(n-1), helper(n+1))
+        
+      return op
+    return helper(n)
+      
+
+class SolutionMemo:
+  def integerReplacement(self, n: int) -> int:
+    memo = {}
+    def helper(x: int) -> int:
+      if x == 1:
+        return 0
+      
+      if x in memo:
+        return memo[x]
+        
+      op = 0
+      if x % 2 == 0:
+        op += helper(x // 2)
+      else:
+        op += min(helper(x-1), helper(x+1))
+        
+      return op
+    return helper(n)
+
+
+class SolutionDP:
+  def integerReplacement(self, n: int) -> int:
+    dp = [0] * (n+2)
+    # dp[1] = 0
     
+    for x in range(n):
+      if x%2 == 0:
+        dp[x] = 1 + dp[x // 2]
+      else:
+        dp[x] = 1 + min(dp[x-1, dp[x+1]])
+    return dp[n]
