0008db50d1e0017e460539fe543ce3c4f1349b14
csandoval18 <christiansandoval1018@outlook.com>
2024-11-16 20:21:55 -0600

    update

---
+++ Algos & DS/DP/practice/medium/416_partition_equal_subset_sum.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+  def canPartition(self, nums: List[int]) -> bool:
+    
+    
+nums = [1,5,11,5]
+# Output: true
+# Explanation: The array can be partitioned as [1,5,5] and [11].
