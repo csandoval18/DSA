61ef230e0eb3541e9e3bfb6cae2c720a0e2db38c
csandoval18 <christiansandoval1018@outlook.com>
2024-12-15 20:57:25 -0600

    update

---
+++ Algos & DS/DP/practice/474_matchsticks_to_square.py
@@ -0,0 +1,24 @@
+from typing import List
+
+'''
+473. Matchsticks to Square (Medium)
+
+You are given an integer array matchsticks where matchsticks[i] is the length of the ith matchstick.
+You want to use all the matchsticks
+
+'''
+
+class Solution:
+  def makesquare(self, matchsticks: List[int]) -> bool:
+    return
+    
+
+# 1.
+matchstricks = [1,1,2,2,2]
+# Output: true
+# Explanation: You can form a square with length 2, one side of the square came two sticks with length 1.
+
+# 2.
+matchsticks = [3,3,3,3,4]
+# Output: false
+# Explanation: You cannot find a way to form a square with all the matchsticks.
