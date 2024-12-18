9284e9074d92cf96e7adfca680b3911e26f85dfe
csandoval18 <christiansandoval1018@outlook.com>
2024-12-18 16:02:34 -0600

    update

--- Algos & DS/DP/practice/medium/474_ones_and_zeroes.py
+++ Algos & DS/DP/practice/medium/474_ones_and_zeroes.py
@@ -13,14 +13,24 @@ from typing import List
 
 class Solution:
   def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
-    def helper(i: int) -> int:
+    def helper(i: int, m: int, n: int) -> int:
       if i >= len(strs):
         return 0
+      if m == 0 and n == 0:
+        return 0
       
+      zeroes = strs.count('0')
+      ones = strs.count('1')
+      new_m, new_n = m - zeroes, n - ones
       
-      
-      
-    return
+      # If idx is not exhausted and new m and n values are non negative
+      take, skip = 0, 0
+      if i < len(strs) and new_m >= 0 and new_n >= 0:
+        take = 1 + helper(i+1, new_m, new_n) # Then add 1 and return
+      else: # Skip current string
+        skip = helper(i+1, m, n)
+        
+      return min(take, skip)
     
         
 
