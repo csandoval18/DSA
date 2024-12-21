d842bedf0926722cac214218982bb4c048f33921
csandoval18 <christiansandoval1018@outlook.com>
2024-12-20 18:18:14 -0600

    update

--- Algos & DS/Sliding Window & 2 Pointer/medium/1423_max_points_you_can_obtain_from_cards.py
+++ Algos & DS/Sliding Window & 2 Pointer/medium/1423_max_points_you_can_obtain_from_cards.py
@@ -1,21 +1,31 @@
 from typing import List
 
+'''
+              _______
+cardPoints = [6,2,3,4,7,2,1,7,1] | k = 4
+lsum = 15, rsum = 0
+              _____           _
+cardPoints = [6,2,3,4,7,2,1,7,1] | k = 4
+lsum = 11, rsum = 1
+              ___           ___
+cardPoints = [6,2,3,4,7,2,1,7,1] | k = 4
+lsum = 8, rsum = 8
+
+'''
 
 class Solution:
   def maxScore(self, cardPoints: List[int], k: int) -> int:
-    rsum = 0
-    lsum = sum(cardPoints[:k])
+    rsum, lsum = 0, sum(cardPoints[:k])
     maxSum = lsum
     
-    j = n-1 # i = left index | j = right index
-    for i in range(k-1, -1, -1):
-      lsum -= cardPoints[i]
-      rsum += cardPoints[j]
-      j = j-1
+    r = len(cardPoints)-1
+    for l in range(k-1, -1, -1):
+      lsum -= cardPoints[l]
+      rsum += cardPoints[r]
+      r = r-1
     
       maxSum = max(maxSum, lsum + rsum)
-    
-    return
+    return maxSum
     
 cardPoints = [1,2,3,4,5,6,1]
 k = 3
---
+++ Algos & DS/Sliding Window & 2 Pointer/medium/3_longest_substring_wout_repeating_chars.py
@@ -0,0 +1,73 @@
+'''
+3. Longest Substring Without Repeating Characters (Medium)
+
+Given a string s, find the length of the longest 
+substring without repeating characters.
+
+
+1. Brute force: generate all substring and find longest with different characters
+2. Better: 
+'''
+
+class SolutionBF:
+  def lengthOfLongestSubstring(self, s: str) -> int:
+    maxLen = 0
+    
+    for i in range(len(s)):
+      seen = set()
+      for j in range(i, len(s)):
+        if s[j] in seen:
+          break
+        
+        maxLen = max(maxLen, j-i+1)
+        seen.add(s[j])
+    return maxLen
+
+
+class SolutionOP:
+  def lengthOfLongestSubstring(self, s: str) -> int:
+    seen = set()
+    l, maxLen = 0, 0
+
+    for r in range(len(s)):
+      while s[r] in seen:
+        seen.remove(s[l])
+        l += 1
+      seen.add(s[r])
+      maxLen = max(maxLen, r-l+1)
+    return maxLen
+        
+class SolutionOP:
+  def lengthOfLongestSubstring(self, s: str) -> int:
+    hm = {}
+    l, r = 0, 0
+    maxLen = 0
+    
+    for r in range(len(s)):
+      if s[r] in hm:
+        if hm[s[r]] >= l:
+          l = hm[s[r]] + 1 # Move l pointer to the stored/previously seen s[r] index + 1 to make window valid
+      
+      maxLen = max(maxLen, r-l+1)
+      hm[s[r]] = r
+    return maxLen
+          
+    
+        
+      
+      
+
+# s = "abcabcbb"
+# Output: 3
+# Explanation: The answer is "abc", with the length of 3.
+
+# s = "bbbbb"
+# Output: 1
+# Explanation: The answer is "b", with the length of 1.
+
+s = "pwwkew"
+# Output: 3
+# Explanation: The answer is "wke", with the length of 3.
+# Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+sol = SolutionBF()
+print(sol.lengthOfLongestSubstring(s))
