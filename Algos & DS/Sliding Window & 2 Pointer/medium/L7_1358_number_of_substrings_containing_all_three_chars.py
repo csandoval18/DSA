5bd67133559a97b7270552c650db766290dd3951
csandoval18 <christiansandoval1018@outlook.com>
2025-01-04 21:15:52 -0600

    update

--- Algos & DS/Sliding Window & 2 Pointer/medium/L7_1358_number_of_substrings_containing_all_three_chars.py
+++ Algos & DS/Sliding Window & 2 Pointer/medium/L7_1358_number_of_substrings_containing_all_three_chars.py
@@ -26,3 +26,41 @@ class SolutionBetter:
             break
       return cnt
       
+      
+class SolutionBetter:
+    def numberOfSubstrings(self, s: str) -> int:
+      lastSeen = [-1, -1, -1]
+      cnt = 0
+      
+      for i in range(len(s)):
+        lastSeen[ord(s[i]) - ord('a')] = i # Update the last seen index for the current char
+        
+        if -1 not in lastSeen: # Check if all chars have been seen 
+          # Add 1 for the found subarray and add the index of the min last seen char to add the total count of 
+          # subarrays that match the condition. For example if the min is in index 2 then we can add 2 because
+          # there are two elements to the left of that index (0, 1, 2)
+          cnt += (1 + min(lastSeen)) 
+      return cnt
+
+class SolutionBetter2:
+    def numberOfSubstrings(self, s: str) -> int:
+      l, cnt = 0, 0
+      K = 3
+      hm = {}
+      
+      for r in range(len(s)):
+        hm[s[r]] = hm.get(hm[s[r]], 0) + 1
+        
+        while len(hm) > K:
+          cnt += len(s) - r
+          
+          if hm[s[l]] == 0:
+            hm.pop(s[l])
+      return cnt
+      
+      
+        
+          
+          
+
+s = 'bacba'
