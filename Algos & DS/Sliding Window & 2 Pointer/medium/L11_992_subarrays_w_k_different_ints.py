55a80b765a579463153e0083f5dae3fd01c1e035
csandoval18 <christiansandoval1018@outlook.com>
2025-01-10 19:35:07 -0600

    update

--- Algos & DS/Sliding Window & 2 Pointer/medium/L11_992_subarrays_w_k_different_ints.py
+++ Algos & DS/Sliding Window & 2 Pointer/medium/L11_992_subarrays_w_k_different_ints.py
@@ -1,7 +1,7 @@
 from typing import List
 
 
-class SolutionBF:
+class SolutionBF: # TC: O(N^2)
   def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
     cnt = 0
     
@@ -15,4 +15,23 @@ class SolutionBF:
           cnt += 1
         elif len(hm) > k:
           break
+    return cnt
+
+
+class SolutionBetter:
+  def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
+    l, cnt = 0, 0
+    hm = {}
+    
+    for r in range(len(nums)):
+      hm[nums[r]] += 1
+      
+      while len(hm) <= k:
+        hm[nums[l]] -= 1
+      
+        if hm[nums] == 0:
+          hm.pop(nums[l])
+        l += 1
+
+      cnt += r-l+1
     return cnt
