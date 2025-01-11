897efc63ef4797c413e3c4256684d11dde20cb84
csandoval18 <christiansandoval1018@outlook.com>
2025-01-10 23:05:16 -0600

    update

--- Algos & DS/Sliding Window & 2 Pointer/medium/L11_992_subarrays_w_k_different_ints.py
+++ Algos & DS/Sliding Window & 2 Pointer/medium/L11_992_subarrays_w_k_different_ints.py
@@ -17,21 +17,29 @@ class SolutionBF: # TC: O(N^2)
           break
     return cnt
 
-
 class SolutionBetter:
   def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
-    l, cnt = 0, 0
-    hm = {}
-    
-    for r in range(len(nums)):
-      hm[nums[r]] += 1
+    def helper(k: int) -> int:
+      l, cnt = 0, 0
+      hm = {}
       
-      while len(hm) <= k:
-        hm[nums[l]] -= 1
-      
-        if hm[nums] == 0:
-          hm.pop(nums[l])
-        l += 1
+      for r in range(len(nums)):
+        hm[nums[r]] = hm.get(nums[r], 0) + 1
+        
+        while len(hm) > k: # Shrink window if distinct count exceeds k
+          hm[nums[l]] -= 1
+        
+          if hm[nums[l]] == 0:
+            del hm[nums[l]]
+          l += 1
 
-      cnt += r-l+1
-    return cnt
+        cnt += r-l+1 # Count subarrays with at most k distinct elements
+      return cnt
+    return helper(k) - helper(k-1)
+  
+nums = [1,2,1,2,3]
+k = 2
+# Explanation: Subarrays formed with exactly 2 different integers: 
+# [1,2], [2,1], [1,2], [2,3], [1,2,1], [2,1,2], [1,2,1,2]
+s = SolutionBetter() 
+print(s.subarraysWithKDistinct(nums, k))
