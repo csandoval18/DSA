ab4a389af23ebbea1f750b229dc2f962abf14e18
csandoval18 <christiansandoval1018@outlook.com>
2025-01-12 17:06:35 -0600

    update

--- Algos & DS/Sliding Window & 2 Pointer/medium/L12_76_minimum_window_substring.py
+++ Algos & DS/Sliding Window & 2 Pointer/medium/L12_76_minimum_window_substring.py
@@ -34,24 +34,56 @@ class SolutionBetter: # TC: O(2N) | SC: O(256)
   def minWindow(self, s: str, t: str) -> str:
     n, m = len(s), len(t)
     hm = {}
+    cnt = 0
     l, sIndex = 0, -1
     minLen = float('inf')
     
-    for c in t:
+    for c in t: # Pre-insert char frequencies of t into the map
       hm[c] = hm.get(c, 0) + 1
     
     for r in range(n):
-      if hm[s[n]] > 0:
+      if hm.get(s[r], 0) > 0:
         cnt += 1
-      hm[s[n]] -= 1
+      hm[s[r]] = hm.get(s[r], 0) - 1
 
       while cnt == m:
         if r-l+1 < minLen:
           minLen = r-l+1
           sIndex = l
           
-        hm[s[l]] -= 1
+        hm[s[l]] = hm.get(s[l], 0) + 1
         if hm[s[l]] > 0:
           cnt += 1
+        l += 1
           
-    return "" if sIndex == -1 else s[sIndex: minLen]
+    return "" if sIndex == -1 else s[sIndex:sIndex + minLen]
+
+
+class SolutionBetter: # TC: O(2N) | SC: O(256)
+  def minWindow(self, s: str, t: str) -> str:
+    n, m = len(s), len(t)
+    hm = {}
+    l, sIndex = 0, -1
+    minLen = float('inf')
+    cnt = 0
+    
+    # Count frequency of characters in t
+    for c in t:
+      hm[c] = hm.get(c, 0) + 1
+    
+    for r in range(n):
+      if hm.get(s[r], 0) > 0:
+        cnt += 1
+      hm[s[r]] = hm.get(s[r], 0) - 1
+      
+      while cnt == m:
+        if r - l + 1 < minLen:
+          minLen = r - l + 1
+          sIndex = l
+        
+        hm[s[l]] = hm.get(s[l], 0) + 1
+        if hm[s[l]] > 0:
+          cnt -= 1
+        l += 1
+  
+    return "" if sIndex == -1 else s[sIndex: sIndex + minLen]
