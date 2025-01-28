af7a0cd2b459fc836a71722a373da1f016f54960
csandoval18 <christiansandoval1018@outlook.com>
2025-01-28 12:21:09 -0600

    update

--- Algos & DS/Bit Manipulation/lessons/L9_Divide_two_ints_wout_using_multiplication_and_division_operators.py
+++ Algos & DS/Bit Manipulation/lessons/L9_Divide_two_ints_wout_using_multiplication_and_division_operators.py
@@ -1,2 +1,24 @@
+# Divide by not using multiplication or division
+
 class Solution:
-  def divide(self, dividend: int, divisor: int) -> int:
+  # Naive using addition
+  '''
+  dividend = 22
+  divisor = 3
+  
+  Keep adding while sum <= dividend
+  3 + 3 + 3 + 3 + 3 + 3 + 3 = 21
+  cnt = 7
+  '''
+  def divide1(self, dividend: int, divisor: int) -> int: # TC: O(dividend) | SC: O(1)
+    curr_sum, cnt = 0, 0
+    
+    while curr_sum + divisor <= dividend:
+      cnt += 1
+      curr_sum += divisor
+    print(cnt)
+
+  def divide2(self, dividend: int, divisor: int) -> int: # TC: O(dividend) | SC: O(1)
+  
+  
+    
