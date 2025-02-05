3818d37a78ca07b7ec86c0c6255b17ed46869b58
csandoval18 <christiansandoval1018@outlook.com>
2025-02-05 12:32:31 -0600

    update

--- Algos & DS/Bit Manipulation/lessons/L8_XOR_of numbers_in_given_range.py
+++ Algos & DS/Bit Manipulation/lessons/L8_XOR_of numbers_in_given_range.py
@@ -44,8 +44,8 @@ n % 4 == 0  res = n
 def f(n: int) -> int:
   if n%4 == 1:
     return 1
-  eli n%4 == 2:
-    return n+1
+  elif n%4 == 2:
+      return n+1
   elif n%4 == 3:
     return 0
   else:
